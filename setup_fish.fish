e9ad7ab1f2fd6e146c1483a779aaeaef2248ada0
eetann <hideharu092@gmail.com>
2019-03-23 20:17:31 +0900

    add new files

--- setup_fish.fish
+++ setup_fish.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
 
-fish_update_completions
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 fisher add oh-my-fish/theme-bobthefish
 fisher add jethrokuan/z
+fish_update_completions
 
