9117f9f922d0aaa99f0bde70eb9df79164950c3c
eetann <hideharu092@gmail.com>
2019-05-09 08:45:07 +0900

    tmux マウス操作とクリップボード

--- fish/config.fish
+++ fish/config.fish
@@ -23,14 +23,3 @@ set -g theme_display_date no
 set -g theme_display_cmd_duration no
 set -g fish_prompt_pwd_dir_length 0
 
-if status --is-login
-	test -n "$TMUX" ;and set change "switch-client" ;or set change "attach-session"
-	if count $argv > /dev/null
-		# 2>/dev/nullでブラックホールに出力を放り込む
-		tmux $change -t $argv[1] 2>/dev/null ;or tmux new-session -d -s $argv[1] ;and tmux $change -t $argv[1]
-	else
-		# -Fでフォーマット指定
-		# #{}でフォーマット文字列
-		set session (tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) ;and tmux $change -t "$session" ;or echo "No sessions found."
-	end
-end
