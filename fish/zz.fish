8623c0e6f46113e767541211ce3f1529e79d924d
eetann <hideharu092@gmail.com>
2019-08-19 15:38:24 +0900

    ファイルの整理

--- fish/zz.fish
+++
@@ -1,7 +0,0 @@
-function zz
-    set dir (z -l $argv | cut -b12- | tac | fzf  --height=60% --select-1 --exit-0 --reverse --no-unicode --preview 'tree -al {} | head -n 100')
-    if [ $dir ]
-        cd $dir
-    end
-    commandline -f repaint
-end
