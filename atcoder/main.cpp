644d60e37af40733a441804838c89d14a64dd7b4
eetann <hideharu092@gmail.com>
2020-10-06 22:13:23 +0900

    AtCoderのテンプレmain.cppのマクロを変更

--- atcoder/main.cpp
+++ atcoder/main.cpp
@@ -2,9 +2,9 @@
 #define INF 1e9
 #define endl '\n'
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
-#define RFOR(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define RFOR(i, a, b) for (int i = (a)-1; i >= (b); --i)
 #define FORE(i, a, b) for (int i = (a), e = (b); i < e; ++i)
-#define RFORE(i, a, b) for (int i = (b)-1, e = (a); i >= e; --i)
+#define RFORE(i, a, b) for (int i = (a)-1, e = (b); i >= e; --i)
 #define ALL(x) (x).begin(), (x).end()
 #define SORT(c) sort((c).begin(), (c).end())
 #define SORTR(c) sort((c).begin(), (c).end(), greater<int>())
