31ecf5d80e5cdcb5c9deb56ab44e154f23cd5fb4
eetann <hideharu092@gmail.com>
2021-08-18 15:24:04 +0900

    🐛

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 sudo apt install -y git build-essential
+echo "Installed build tools."
 git clone https://github.com/eetann/dotfiles.git $HOME/dotfiles
 cd $HOME/dotfiles
+echo "Got dotfiles."
 make install
