667b49154137e44879728980154b3c102c24fcb4
eetann <hideharu092@gmail.com>
2022-03-09 16:35:59 +0900

    delete unnecessary file

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sudo apt install -y git build-essential
-echo "Installed build tools."
-git clone https://github.com/eetann/dotfiles.git $HOME/dotfiles
-cd $HOME/dotfiles
-echo "Got dotfiles."
-make install
