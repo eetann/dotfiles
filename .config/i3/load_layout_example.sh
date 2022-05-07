5ede75a0a0075faf8fbc9804633144ddaa6bee34
eetann <hideharu092@gmail.com>
2022-05-07 11:05:38 +0900

    change layout and autostart app

--- .config/i3/load_layout_example.sh
+++ .config/i3/load_layout_example.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
 # cp ~/dotfiles/.config/i3/load_layout_example.sh ~/load_layout.sh
-# mkdir -p ~/.i3
-# cp ~/dotfiles/.config/i3/workspace_1.json ~/.i3/workspace_1.json
-# cp ~/dotfiles/.config/i3/workspace_2.json ~/.i3/workspace_2.json
 # chmod u+x ~/load_layout.sh
 
-# First we append the saved layout of worspace N to workspace M
-i3-msg "workspace 1; append_layout ~/.i3/workspace_1.json"
-i3-msg "workspace 2; append_layout ~/.i3/workspace_2.json"
+i3-msg "workspace 1; append_layout ~/dotfiles/.config/i3/workspace_1.json"
+(LANG=ja_jp.UTF8 vivaldi-stable --profile-directory=Default --app-id=ddiddklncfgbfaaahngklemobghhjkim &)
+(wezterm &)
+i3-msg '[class="^org\\.wezfurlong\\.wezterm$"] focus'
 
-# And finally we fill the containers with the programs they had
-(LANG=ja_jp.UTF8 vivaldi-stable --profile-directory=Default --app-id=xxxxxxxxxxxxxxxxxxx &)
-(alacritty &)
+i3-msg "workspace 2; append_layout ~/dotfiles/.config/i3/workspace_2.json"
 (LANG=ja_jp.UTF8 vivaldi-stable --profile-directory=Default &)
-i3-msg '[title="Alacritty"] focus'
 i3-msg 'workspace 2'
