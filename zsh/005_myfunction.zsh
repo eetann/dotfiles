7e86fd5dfa75c997fe14a04c17a59c7148ff038e
eetann <hideharu092@gmail.com>
2022-06-19 14:12:39 +0900

    add fzf height option

--- zsh/005_myfunction.zsh
+++ zsh/005_myfunction.zsh
@@ -151,7 +151,7 @@ function fzf_npm_scripts() {
     zle send-break
     return 1
   fi
-  local selected=`echo $scripts | FZF_DEFAULT_OPTS='' fzf --reverse --exit-0 | awk -F ' = ' '{ print $1}'`
+  local selected=`echo $scripts | FZF_DEFAULT_OPTS='' fzf --height=50% --reverse --exit-0 | awk -F ' = ' '{ print $1}'`
 
   zle reset-prompt
   if [[ -z $selected ]]; then
