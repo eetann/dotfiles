d4c75426c5bb80ddd1b8cd7dd03dea233961531e
eetann <hideharu092@gmail.com>
2022-07-04 20:23:08 +0900

    format

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,27 +13,27 @@ local function append_array(array, other)
 end
 
 local key_table = {
-  { key = ";", mods = "CTRL", action = "IncreaseFontSize" },
-  { key = "+", mods = "CTRL", action = "IncreaseFontSize" },
-  { key = "-", mods = "CTRL", action = "DecreaseFontSize" },
-  { key = "0", mods = "CTRL", action = "ResetFontSize" },
+  { key = ';', mods = 'CTRL', action = 'IncreaseFontSize' },
+  { key = '+', mods = 'CTRL', action = 'IncreaseFontSize' },
+  { key = '-', mods = 'CTRL', action = 'DecreaseFontSize' },
+  { key = '0', mods = 'CTRL', action = 'ResetFontSize' },
 }
 
-local mac = wezterm.target_triple:find("darwin")
+local mac = wezterm.target_triple:find('darwin')
 
 if mac then
   local mac_key_table = {
-    {key="n", mods="CMD", action={SendKey={key="n", mods="ALT"}}},
-    {key="p", mods="CMD", action={SendKey={key="p", mods="ALT"}}},
+    { key = 'n', mods = 'CMD', action = { SendKey = { key = 'n', mods = 'ALT' } } },
+    { key = 'p', mods = 'CMD', action = { SendKey = { key = 'p', mods = 'ALT' } } },
   }
   append_array(key_table, mac_key_table)
 end
 
 return {
-  font = wezterm.font("HackGenNerd Console"),
+  font = wezterm.font('HackGenNerd Console'),
   use_ime = true,
   font_size = 16,
-  color_scheme = "Gruvbox Dark",
+  color_scheme = 'Gruvbox Dark',
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
   keys = key_table,
