1c9b52ec8000b8225e054de5058a40686c556cff
eetann <hideharu092@gmail.com>
2022-07-05 21:47:22 +0900

    add

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,9 @@
 -- WIP
-require('rc/base') -- ./lua/rc/base.lua
+require('rc/base') -- .config/nvim/lua/rc/base.lua
+require('rc/plugin') -- .config/nvim/lua/rc/plugin.lua
+-- " 設定ファイルの読み込み------------------------------------------
+-- call map(sort(split(glob('~/dotfiles/vim/rc/*.vim'), '\n')),
+-- \ {->[execute('exec "so" v:val')]})
+-- " 参考
+-- " echo map([1, 2, 3], {-> v:val + v:val })
+-- " {args -> expr1} lambda
---
+++ .config/nvim/lua/rc/plugin.lua
@@ -0,0 +1,20 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local packer_bootstrap
+---@diagnostic disable-next-line: missing-parameter
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+    install_path })
+end
+
+return require('packer').startup(function(use)
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
