b5e8318446b7d11adf6be7fd8598656d4a5e0edf
eetann <hideharu092@gmail.com>
2022-09-04 14:59:05 +0900

    fix mapping to run lazygit

--- .config/nvim/lua/plug/toggleterm.lua
+++ .config/nvim/lua/plug/toggleterm.lua
@@ -13,27 +13,3 @@ vim.keymap.set("n", "<Leader>tt", "<Cmd>ToggleTerm<CR>")
 vim.keymap.set("n", "<Leader>t-", "<Cmd>ToggleTerm direction=horizontal<CR>")
 vim.keymap.set("n", "<Leader>t\\", "<Cmd>ToggleTerm direction=vertical<CR>")
 vim.keymap.set("n", "<Leader>tf", "<Cmd>ToggleTerm direction=float<CR>")
-
-local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({
-	cmd = 'lazygit --use-config-dir="$HOME/.config/lazygit"',
-	dir = "git_dir",
-	direction = "float",
-	float_opts = {
-		border = "double",
-		width = function()
-			return vim.fn.floor(vim.o.columns * 0.95)
-		end,
-		height = function()
-			return vim.fn.floor(vim.o.lines * 0.9)
-		end,
-	},
-	on_open = function(term)
-		vim.cmd("startinsert!")
-		vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-	end,
-})
-
-vim.keymap.set("n", "<leader>tg", function()
-	lazygit:toggle()
-end, { noremap = true, silent = true })
