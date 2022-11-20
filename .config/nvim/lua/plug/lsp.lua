34e5648536c73761f1cf15f2cd230b0a2ea947a7
eetann <hideharu092@gmail.com>
2022-11-20 17:17:33 +0900

    add ESLint filetypes

--- .config/nvim/lua/plug/lsp.lua
+++ .config/nvim/lua/plug/lsp.lua
@@ -185,7 +185,21 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
 local null_ls = require("null-ls")
 local sources = {
 	null_ls.builtins.code_actions.eslint,
-	null_ls.builtins.diagnostics.eslint,
+	null_ls.builtins.diagnostics.eslint.with({
+		filetypes = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+			"vue",
+			"css",
+			"html",
+			"json",
+			"jsonc",
+			"json5",
+			"yaml",
+		},
+	}),
 	null_ls.builtins.formatting.prettier.with({
 		filetypes = {
 			"javascript",
