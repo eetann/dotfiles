bc31d9d9ed928d0331f9cf049ee13f7e065a10d6
eetann <hideharu092@gmail.com>
2023-01-22 21:58:28 +0900

    feat: update mappings for lspsaga

--- .config/nvim/lua/plug/lsp.lua
+++ .config/nvim/lua/plug/lsp.lua
@@ -1,3 +1,8 @@
+vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+
 require("symbols-outline").setup()
 vim.keymap.set("n", "<Leader>o", "<Cmd>SymbolsOutline<CR>", { noremap = true })
 
@@ -9,110 +14,28 @@ require("illuminate").configure({
 local saga = require("lspsaga")
 
 saga.setup({
-	-- Options with default value
-	-- "single" | "double" | "rounded" | "bold" | "plus"
-	border_style = "single",
-	--the range of 0 for fully opaque window (disabled) to 100 for fully
-	--transparent background. Values between 0-30 are typically most useful.
-	saga_winblend = 0,
-	-- when cursor in saga window you config these to move
-	move_in_saga = { prev = "<C-u>", next = "<C-d>" },
-	-- Error, Warn, Info, Hint
-	-- use emoji like
-	-- { "🙀", "😿", "😾", "😺" }
-	-- or
-	-- { "😡", "😥", "😤", "😐" }
-	-- and diagnostic_header can be a function type
-	-- must return a string and when diagnostic_header
-	-- is function type it will have a param `entry`
-	-- entry is a table type has these filed
-	-- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
-	diagnostic_header = { " ", " ", " ", "ﴞ " },
-	-- preview lines above of lsp_finder
-	preview_lines_above = 0,
-	-- preview lines of lsp_finder and definition preview
-	max_preview_lines = 10,
-	-- use emoji lightbulb in default
-	code_action_icon = "💡",
-	-- if true can press number to execute the codeaction in codeaction window
-	code_action_num_shortcut = true,
-	-- same as nvim-lightbulb but async
-	code_action_lightbulb = {
-		enable = true,
-		enable_in_insert = true,
-		cache_code_action = true,
-		sign = true,
-		update_time = 150,
-		sign_priority = 20,
-		virtual_text = true,
+	scroll_preview = {
+		scroll_down = "<C-d>",
+		scroll_up = "<C-u>",
 	},
-	-- finder icons
-	finder_icons = {
-		def = "  ",
-		ref = "諭 ",
-		link = "  ",
-	},
-	-- finder do lsp request timeout
-	-- if your project big enough or your server very slow
-	-- you may need to increase this value
-	finder_request_timeout = 1500,
-	finder_action_keys = {
-		open = { "o", "<CR>" },
-		vsplit = "s",
-		split = "i",
+	finder = {
+		edit = { "o", "<CR>" },
+		vsplit = { "\\", "|", "<C-v>" },
+		split = { "-", "<C-x>" },
 		tabe = "t",
 		quit = { "q", "<ESC>" },
 	},
-	code_action_keys = {
+	definition = {
+		edit = "o",
+		vsplit = "<C-v>",
+		split = "<C-x>",
+		tabe = "t",
 		quit = "q",
-		exec = "<CR>",
+		close = "<Esc>",
 	},
-	definition_action_keys = {
-		edit = "<C-c>o",
-		vsplit = "<C-c>v",
-		split = "<C-c>x",
-		tabe = "<C-c>t",
-		quit = "q",
+	lightbulb = {
+		enable_in_insert = false,
 	},
-	rename_action_quit = "<C-c>",
-	rename_in_select = true,
-	-- show symbols in winbar must nightly
-	-- in_custom mean use lspsaga api to get symbols
-	-- and set it to your custom winbar or some winbar plugins.
-	-- if in_cusomt = true you must set in_enable to false
-	symbol_in_winbar = {
-		in_custom = false,
-		enable = true,
-		separator = " ",
-		show_file = true,
-		-- define how to customize filename, eg: %:., %
-		-- if not set, use default value `%:t`
-		-- more information see `vim.fn.expand` or `expand`
-		-- ## only valid after set `show_file = true`
-		file_formatter = "",
-		click_support = false,
-	},
-	-- show outline
-	show_outline = {
-		win_position = "right",
-		--set special filetype win that outline window split.like NvimTree neotree
-		-- defx, db_ui
-		win_with = "",
-		win_width = 30,
-		auto_enter = true,
-		auto_preview = true,
-		virt_text = "┃",
-		jump_key = "o",
-		-- auto refresh when change buffer
-		auto_refresh = true,
-	},
-	-- custom lsp kind
-	-- usage { Field = 'color code'} or {Field = {your icon, your color code}}
-	custom_kind = {},
-	-- if you don't use nvim-lspconfig you must pass your server name and
-	-- the related filetypes into this table
-	-- like server_filetype_map = { metals = { "sbt", "scala" } }
-	server_filetype_map = {},
 })
 
 local on_attach = function(client, bufnr)
@@ -120,13 +43,15 @@ local on_attach = function(client, bufnr)
 	vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
 	vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", bufopts)
 	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", bufopts)
-	vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", bufopts)
+	vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", bufopts)
 	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", bufopts)
+	vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>", bufopts)
 	vim.keymap.set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", bufopts)
-	vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", bufopts)
+	vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.signature_help()<CR>", bufopts)
 	vim.keymap.set("n", "<C-g>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", bufopts)
 	vim.keymap.set("i", "<C-g>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", bufopts)
-	vim.keymap.set("n", "gh", "<cmd>Lspsaga signature_help<CR>", bufopts)
+	vim.keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", bufopts)
+	vim.keymap.set("n", "gs", "<cmd>Lspsaga signature_help<CR>", bufopts)
 	vim.keymap.set("n", "<leader>cr", "<cmd>Lspsaga rename<CR>", bufopts)
 	vim.keymap.set("n", "<space>ca", "<cmd>CodeActionMenu<CR>", bufopts)
 	vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", bufopts)
