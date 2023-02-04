4ebdb2cf7a8422b18acf73c827b027b2e89bc57e
eetann <hideharu092@gmail.com>
2023-02-04 20:16:46 +0900

    fix: gitsigns mappings for jumpto-diffs

--- .config/nvim/lua/plug/gitsigns.lua
+++ .config/nvim/lua/plug/gitsigns.lua
@@ -41,8 +41,24 @@ require("gitsigns").setup({
 	on_attach = function()
 		local gs = package.loaded.gitsigns
 
-		vim.keymap.set("n", "]c", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-		vim.keymap.set("n", "[c", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+		vim.keymap.set("n", "]c", function()
+			if vim.wo.diff then
+				return "]c"
+			end
+			vim.schedule(function()
+				gs.next_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+		vim.keymap.set("n", "[c", function()
+			if vim.wo.diff then
+				return "[c"
+			end
+			vim.schedule(function()
+				gs.prev_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
 		vim.keymap.set("n", "<Leader>sb", gs.toggle_current_line_blame)
 		vim.keymap.set("n", "<Leader>gb", gs.toggle_current_line_blame)
 		vim.keymap.set("n", "<Leader>gB", function()
