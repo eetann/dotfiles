d46b0cfb67119d9c446f0bf00c222f1359b283e3
eetann <hideharu092@gmail.com>
2023-03-06 15:10:33 +0900

    feat: add commit message completion

--- .config/nvim/lua/rc/filetype.lua
+++ .config/nvim/lua/rc/filetype.lua
@@ -82,16 +82,6 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 	end,
 })
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-	group = group_name,
-	pattern = { "gitcommit" },
-	callback = function()
-		vim.cmd([[
-    call feedkeys('I', 'n')
-    ]])
-	end,
-})
-
 vim.api.nvim_create_autocmd({ "BufRead" }, {
 	group = group_name,
 	pattern = { "/tmp/*" },
@@ -103,4 +93,36 @@ vim.api.nvim_create_autocmd({ "BufRead" }, {
 vim.api.nvim_create_autocmd({ "TermOpen" }, {
 	group = group_name,
 	command = "startinsert",
+})
+
+-- gitcommit settings
+local git_commit_prefixs = {
+	{ word = "feat: ", menu = "機能追加", kind = "pre" },
+	{ word = "fix: ", menu = "バグ修正", kind = "pre" },
+	{ word = "docs: ", menu = "ドキュメント", kind = "pre" },
+	{ word = "style: ", menu = "デザイン変更のみ", kind = "pre" },
+	{ word = "refactor: ", menu = "リファクタリング ", kind = "pre" },
+	{ word = "perf: ", menu = "パフォーマンス改善 ", kind = "pre" },
+	{ word = "test: ", menu = "テスト関連の変更 ", kind = "pre" },
+	{ word = "build: ", menu = "ビルドシステムの変更", kind = "pre" },
+	{ word = "ci: ", menu = "CI関連の変更", kind = "pre" },
+	{ word = "chore: ", menu = "その他の変更", kind = "pre" },
+}
+
+---@diagnostic disable-next-line: undefined-field
+_G.complete_git_commit = function()
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	if row == 1 and col == 0 then
+		vim.fn.complete(col + 1, git_commit_prefixs)
+	end
+	return ""
+end
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	group = group_name,
+	pattern = { "gitcommit" },
+	callback = function()
+		vim.cmd("startinsert")
+		vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-R>=v:lua.complete_git_commit()<CR>", true, true, true))
+	end,
 })
