c1848e9453b826e0fbf312ec111c6c6ba8cae927
eetann <hideharu092@gmail.com>
2023-11-19 11:42:39 +0900

    feat: lspsagaを更新

--- .config/nvim/lua/plug/lsp.lua
+++ .config/nvim/lua/plug/lsp.lua
@@ -7,207 +7,213 @@ require("symbols-outline").setup()
 vim.keymap.set("n", "<Leader>o", "<Cmd>SymbolsOutline<CR>", { noremap = true })
 
 require("illuminate").configure({
-	modes_allowlist = { "n" },
-	delay = 100,
+  modes_allowlist = { "n" },
+  delay = 100,
 })
 
 local saga = require("lspsaga")
 
 saga.setup({
-	scroll_preview = {
-		scroll_down = "<C-d>",
-		scroll_up = "<C-u>",
-	},
-	finder = {
-		edit = { "o", "<CR>" },
-		vsplit = { "\\", "|", "<C-v>" },
-		split = { "-", "<C-x>" },
-		tabe = "t",
-		quit = { "q", "<ESC>" },
-	},
-	definition = {
-		edit = "o",
-		vsplit = "<C-v>",
-		split = "<C-x>",
-		tabe = "t",
-		quit = "q",
-		close = "<Esc>",
-	},
-	lightbulb = {
-		enable_in_insert = false,
-	},
-	diagnostic = {
-		jump_num_shortcut = false,
-	},
+  scroll_preview = {
+    scroll_down = "<C-d>",
+    scroll_up = "<C-u>",
+  },
+  finder = {
+    edit = { "o", "<CR>" },
+    vsplit = { "\\", "|", "<C-v>" },
+    split = { "-", "<C-x>" },
+    tabe = "t",
+    quit = { "q", "<ESC>" },
+  },
+  definition = {
+    edit = "o",
+    vsplit = "<C-v>",
+    split = "<C-x>",
+    tabe = "t",
+    quit = "q",
+    close = "<Esc>",
+  },
+  lightbulb = {
+    enable_in_insert = false,
+  },
+  diagnostic = {
+    jump_num_shortcut = false,
+    keys = {
+      exec_action = "o",
+      quit = "q",
+      toggle_or_jump = "<CR>",
+      quit_in_show = { "q", "<Esc>" },
+    },
+  },
 })
 
 local on_attach = function(client, bufnr)
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-	vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", bufopts)
-	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", bufopts)
-	vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", bufopts)
-	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", bufopts)
-	vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>", bufopts)
-	vim.keymap.set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", bufopts)
-	vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", bufopts)
-	vim.keymap.set("n", "<C-g>", "<cmd>Lspsaga hover_doc<CR>", bufopts)
-	vim.keymap.set("i", "<C-g>h", "<cmd>Lspsaga hover_doc<CR>", bufopts)
-	vim.keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", bufopts)
-	vim.keymap.set("n", "gh", "<cmd>Lspsaga hover_doc<CR>", bufopts)
-	vim.keymap.set("n", "<leader>cr", "<cmd>Lspsaga rename<CR>", bufopts)
-	vim.keymap.set("n", "<space>ca", "<cmd>CodeActionMenu<CR>", bufopts)
-	vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", bufopts)
-	vim.keymap.set("n", "<leader>cc", "<cmd>Lspsaga show_cursor_diagnostics<CR>", bufopts)
-	vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", bufopts)
-	vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", bufopts)
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+  vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", bufopts)
+  vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", bufopts)
+  vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", bufopts)
+  vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", bufopts)
+  vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>", bufopts)
+  vim.keymap.set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", bufopts)
+  vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", bufopts)
+  vim.keymap.set("n", "<C-g>", "<cmd>Lspsaga hover_doc<CR>", bufopts)
+  vim.keymap.set("i", "<C-g>h", "<cmd>Lspsaga hover_doc<CR>", bufopts)
+  vim.keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", bufopts)
+  vim.keymap.set("n", "gh", "<cmd>Lspsaga hover_doc<CR>", bufopts)
+  vim.keymap.set("n", "<leader>cr", "<cmd>Lspsaga rename<CR>", bufopts)
+  vim.keymap.set("n", "<space>ca", "<cmd>CodeActionMenu<CR>", bufopts)
+  vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", bufopts)
+  vim.keymap.set("n", "<leader>cc", "<cmd>Lspsaga show_cursor_diagnostics<CR>", bufopts)
+  vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", bufopts)
+  vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", bufopts)
 
-	if client.server_capabilities.documentFormattingProvider then
-		vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-			group = "my_nvim_rc",
-			buffer = bufnr,
-			callback = function()
-				vim.lsp.buf.format({ timeout_ms = 2000 })
-			end,
-		})
-	end
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+      group = "my_nvim_rc",
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ timeout_ms = 2000 })
+      end,
+    })
+  end
 end
 
 local function on_attach_disable_format(client, buffer)
-	client.server_capabilities.documentFormattingProvider = false
-	on_attach(client, buffer)
+  client.server_capabilities.documentFormattingProvider = false
+  on_attach(client, buffer)
 end
 
 require("mason").setup()
 local nvim_lsp = require("lspconfig")
 
 local function detected_root_dir(root_dir)
-	return not not (root_dir(vim.api.nvim_buf_get_name(0), vim.api.nvim_get_current_buf()))
+  return not not (root_dir(vim.api.nvim_buf_get_name(0), vim.api.nvim_get_current_buf()))
 end
 
 require("neodev").setup({})
 local mason_lspconfig = require("mason-lspconfig")
 mason_lspconfig.setup_handlers({
-	function(server_name)
-		local opts = {}
-		opts.on_attach = on_attach
-		opts.capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-		opts.capabilities.textDocument.completion.completionItem.snippetSupport = true
+  function(server_name)
+    local opts = {}
+    opts.on_attach = on_attach
+    opts.capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+    opts.capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-		if server_name == "tsserver" then
-			local root_dir = nvim_lsp.util.root_pattern("package.json", "node_modules")
-			opts.root_dir = root_dir
-			opts.autostart = detected_root_dir(root_dir)
-			opts.on_attach = on_attach_disable_format
-		elseif server_name == "denols" then
-			local root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc", "deps.ts")
-			opts.root_dir = root_dir
-			opts.autostart = detected_root_dir(root_dir)
-			opts.init_options = { lint = true, unstable = true }
-		elseif server_name == "sumneko_lua" then
-			opts.on_attach = on_attach_disable_format
-			opts.settings = {
-				Lua = {
-					completion = {
-						callSnippet = "Replace",
-					},
-					workspace = {
-						library = vim.api.nvim_get_runtime_file("", true),
-						checkThirdParty = false,
-					},
-				},
-			}
-		end
+    if server_name == "tsserver" then
+      local root_dir = nvim_lsp.util.root_pattern("package.json", "node_modules")
+      opts.root_dir = root_dir
+      opts.autostart = detected_root_dir(root_dir)
+      opts.on_attach = on_attach_disable_format
+    elseif server_name == "denols" then
+      local root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc", "deps.ts")
+      opts.root_dir = root_dir
+      opts.autostart = detected_root_dir(root_dir)
+      opts.init_options = { lint = true, unstable = true }
+    elseif server_name == "sumneko_lua" then
+      opts.on_attach = on_attach_disable_format
+      opts.settings = {
+        Lua = {
+          completion = {
+            callSnippet = "Replace",
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false,
+          },
+        },
+      }
+    end
 
-		nvim_lsp[server_name].setup(opts)
-	end,
+    nvim_lsp[server_name].setup(opts)
+  end,
 })
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	update_in_insert = false,
-	virtual_text = {
-		format = function(diagnostic)
-			return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
-		end,
-	},
+  update_in_insert = false,
+  virtual_text = {
+    format = function(diagnostic)
+      return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
+    end,
+  },
 })
 
 local null_ls = require("null-ls")
 
 local eslint_condition = function(utils)
-	return utils.root_has_file({
-		".eslintrc.js",
-		".eslintrc.cjs",
-		".eslintrc.yaml",
-		".eslintrc.yml",
-		".eslintrc.json",
-	})
+  return utils.root_has_file({
+    ".eslintrc.js",
+    ".eslintrc.cjs",
+    ".eslintrc.yaml",
+    ".eslintrc.yml",
+    ".eslintrc.json",
+  })
 end
 
 local prettier_condition = function(utils)
-	return utils.root_has_file({
-		".prettierrc",
-		".prettierrc.json",
-		".prettierrc.yml",
-		".prettierrc.yaml",
-		".prettierrc.json5",
-		".prettierrc.js",
-		".prettierrc.cjs",
-		"prettier.config.js",
-		"prettier.config.cjs",
-		".prettierrc.toml",
-	})
+  return utils.root_has_file({
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.yml",
+    ".prettierrc.yaml",
+    ".prettierrc.json5",
+    ".prettierrc.js",
+    ".prettierrc.cjs",
+    "prettier.config.js",
+    "prettier.config.cjs",
+    ".prettierrc.toml",
+  })
 end
 
 local sources = {
-	null_ls.builtins.code_actions.eslint_d.with({
-		condition = eslint_condition,
-		extra_filetypes = { "json5", "astro" },
-	}),
-	null_ls.builtins.diagnostics.eslint_d.with({
-		condition = eslint_condition,
-		extra_filetypes = { "json5", "astro" },
-	}),
-	null_ls.builtins.formatting.prettier.with({
-		condition = prettier_condition,
-		extra_filetypes = { "json5", "astro" },
-	}),
-	null_ls.builtins.diagnostics.textlint.with({
-		filetypes = { "markdown" },
-		condition = function(utils)
-			local is_mdn = utils.root_matches("translated%-content")
-			local is_not_dotfiles = not utils.root_matches("dotfiles")
-			return is_mdn
-				or (
-					is_not_dotfiles
-					and utils.root_has_file({
-						".textlintrc",
-						".textlintrc.js",
-						".textlintrc.json",
-						".textlintrc.yml",
-						".textlintrc.yaml",
-					})
-				)
-		end,
-		cwd = function(params)
-			local is_mdn = params.root:find("translated%-content")
-			return is_mdn and vim.fn.expand("~/ghq/github.com/mozilla-japan/translation/MDN/textlint")
-		end,
-	}),
-	null_ls.builtins.diagnostics.shellcheck.with({
-		condition = function()
-			---@diagnostic disable-next-line: missing-parameter
-			return vim.fn.expand("%:t") ~= ".envrc"
-		end,
-	}),
-	null_ls.builtins.code_actions.shellcheck,
-	null_ls.builtins.formatting.shfmt,
-	null_ls.builtins.formatting.stylua,
+  null_ls.builtins.code_actions.eslint_d.with({
+    condition = eslint_condition,
+    extra_filetypes = { "json5", "astro" },
+  }),
+  null_ls.builtins.diagnostics.eslint_d.with({
+    condition = eslint_condition,
+    extra_filetypes = { "json5", "astro" },
+  }),
+  null_ls.builtins.formatting.prettier.with({
+    condition = prettier_condition,
+    extra_filetypes = { "json5", "astro" },
+  }),
+  null_ls.builtins.diagnostics.textlint.with({
+    filetypes = { "markdown" },
+    condition = function(utils)
+      local is_mdn = utils.root_matches("translated%-content")
+      local is_not_dotfiles = not utils.root_matches("dotfiles")
+      return is_mdn
+          or (
+            is_not_dotfiles
+            and utils.root_has_file({
+              ".textlintrc",
+              ".textlintrc.js",
+              ".textlintrc.json",
+              ".textlintrc.yml",
+              ".textlintrc.yaml",
+            })
+          )
+    end,
+    cwd = function(params)
+      local is_mdn = params.root:find("translated%-content")
+      return is_mdn and vim.fn.expand("~/ghq/github.com/mozilla-japan/translation/MDN/textlint")
+    end,
+  }),
+  null_ls.builtins.diagnostics.shellcheck.with({
+    condition = function()
+      ---@diagnostic disable-next-line: missing-parameter
+      return vim.fn.expand("%:t") ~= ".envrc"
+    end,
+  }),
+  null_ls.builtins.code_actions.shellcheck,
+  null_ls.builtins.formatting.shfmt,
+  null_ls.builtins.formatting.stylua,
 }
 null_ls.setup({
-	sources = sources,
-	diagnostics_format = "#{m} (#{s}: #{c})",
-	on_attach = function(client, buffer)
-		on_attach(client, buffer)
-	end,
+  sources = sources,
+  diagnostics_format = "#{m} (#{s}: #{c})",
+  on_attach = function(client, buffer)
+    on_attach(client, buffer)
+  end,
 })
--- .config/nvim/lua/rc/plugin.lua
+++ .config/nvim/lua/rc/plugin.lua
@@ -1,181 +1,181 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--single-branch",
-		"https://github.com/folke/lazy.nvim.git",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
 end
 vim.opt.runtimepath:prepend(lazypath)
 
 local function conf(name)
-	return function()
-		require("plug/" .. name)
-	end
+  return function()
+    require("plug/" .. name)
+  end
 end
 
 local colorscheme = "nightfox.nvim"
 
 require("lazy").setup({
-	-- .config/nvim/lua/plug/color-scheme.lua
-	{ "EdenEast/nightfox.nvim", config = conf("color-scheme") },
+  -- .config/nvim/lua/plug/color-scheme.lua
+  { "EdenEast/nightfox.nvim",        config = conf("color-scheme") },
 
-	{ "nvim-lua/plenary.nvim" },
+  { "nvim-lua/plenary.nvim" },
 
-	{ "machakann/vim-highlightedyank", config = conf("vim-highlightedyank") },
+  { "machakann/vim-highlightedyank", config = conf("vim-highlightedyank") },
 
-	{ "echasnovski/mini.ai", version = "*", config = conf("mini-ai") },
-	{ "echasnovski/mini.surround", version = "*", config = conf("mini-surround") },
+  { "echasnovski/mini.ai",           version = "*",                                      config = conf("mini-ai") },
+  { "echasnovski/mini.surround",     version = "*",                                      config = conf("mini-surround") },
 
-	{ "machakann/vim-swap", config = conf("vim-swap") },
-	{ "jiangmiao/auto-pairs", config = conf("auto-pairs") },
+  { "machakann/vim-swap",            config = conf("vim-swap") },
+  { "jiangmiao/auto-pairs",          config = conf("auto-pairs") },
 
-	{ "nvim-telescope/telescope.nvim", config = conf("telescope") },
-	{ "johmsalas/text-case.nvim", dependencies = { "nvim-telescope/telescope.nvim" }, config = conf("text-case") },
+  { "nvim-telescope/telescope.nvim", config = conf("telescope") },
+  { "johmsalas/text-case.nvim",      dependencies = { "nvim-telescope/telescope.nvim" }, config = conf("text-case") },
 
-	-- .config/nvim/lua/plug/treesitter.lua
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = conf("treesitter"),
-		dependencies = { colorscheme },
-	},
-	{ "nvim-treesitter/nvim-treesitter-context", dependencies = { "nvim-treesitter/nvim-treesitter" } },
-	{ "nvim-tree/nvim-web-devicons" },
+  -- .config/nvim/lua/plug/treesitter.lua
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = conf("treesitter"),
+    dependencies = { colorscheme },
+  },
+  { "nvim-treesitter/nvim-treesitter-context", dependencies = { "nvim-treesitter/nvim-treesitter" } },
+  { "nvim-tree/nvim-web-devicons" },
   { "neovim/tree-sitter-vimdoc" },
-	{
-		"akinsho/bufferline.nvim",
-		version = "v2.*",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = conf("bufferline"),
-	},
-	{ "norcalli/nvim-colorizer.lua", config = conf("nvim-colorizer") },
-	{
-		"lukas-reineke/indent-blankline.nvim",
+  {
+    "akinsho/bufferline.nvim",
+    version = "v2.*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = conf("bufferline"),
+  },
+  { "norcalli/nvim-colorizer.lua",      config = conf("nvim-colorizer") },
+  {
+    "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-		config = conf("indent-blankline"),
-		dependencies = { "nvim-treesitter/nvim-treesitter", colorscheme },
-	},
-	{ "andymass/vim-matchup", dependencies = { "nvim-treesitter/nvim-treesitter" } }, -- %を拡張
+    config = conf("indent-blankline"),
+    dependencies = { "nvim-treesitter/nvim-treesitter", colorscheme },
+  },
+  { "andymass/vim-matchup",             dependencies = { "nvim-treesitter/nvim-treesitter" } }, -- %を拡張
 
-	-- .config/nvim/lua/plug/lsp.lua
-	{ "neovim/nvim-lspconfig", config = conf("lsp") },
-	{ "williamboman/mason.nvim" },
-	{ "williamboman/mason-lspconfig.nvim" },
-	-- { "ray-x/lsp_signature.nvim" },
-	{ "glepnir/lspsaga.nvim", dependencies = { "nvim-treesitter/nvim-treesitter" } },
-	{ "nvimtools/none-ls.nvim" },
-	{ "simrat39/symbols-outline.nvim" },
-	{ "weilbith/nvim-code-action-menu" },
-	{ "folke/neodev.nvim" },
-	{ "folke/lsp-colors.nvim" },
-	{ "RRethy/vim-illuminate" },
+  -- .config/nvim/lua/plug/lsp.lua
+  { "neovim/nvim-lspconfig",            config = conf("lsp") },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  -- { "ray-x/lsp_signature.nvim" },
+  { "nvimdev/lspsaga.nvim",             dependencies = { "nvim-treesitter/nvim-treesitter" } },
+  { "nvimtools/none-ls.nvim" },
+  { "simrat39/symbols-outline.nvim" },
+  { "weilbith/nvim-code-action-menu" },
+  { "folke/neodev.nvim" },
+  { "folke/lsp-colors.nvim" },
+  { "RRethy/vim-illuminate" },
 
-	-- .config/nvim/lua/plug/completion.lua
-	{ "SirVer/ultisnips", dependencies = { "honza/vim-snippets", "quangnguyen30192/cmp-nvim-ultisnips" } },
-	{
-		"hrsh7th/nvim-cmp",
-		config = conf("completion"),
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"uga-rosa/cmp-dictionary",
-			"onsails/lspkind-nvim",
-		},
-	},
+  -- .config/nvim/lua/plug/completion.lua
+  { "SirVer/ultisnips",                 dependencies = { "honza/vim-snippets", "quangnguyen30192/cmp-nvim-ultisnips" } },
+  {
+    "hrsh7th/nvim-cmp",
+    config = conf("completion"),
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "uga-rosa/cmp-dictionary",
+      "onsails/lspkind-nvim",
+    },
+  },
 
-	{ "petertriho/nvim-scrollbar", config = conf("nvim-scrollbar") },
+  { "petertriho/nvim-scrollbar", config = conf("nvim-scrollbar") },
 
-	{ "MunifTanjim/nui.nvim" },
-	{ "rcarriga/nvim-notify" },
-	{
-		"folke/noice.nvim",
-		config = conf("noice"),
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-	},
+  { "MunifTanjim/nui.nvim" },
+  { "rcarriga/nvim-notify" },
+  {
+    "folke/noice.nvim",
+    config = conf("noice"),
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
 
-	{ "lambdalisue/vim-quickrun-neovim-job" },
-	{ "thinca/vim-quickrun", config = conf("vim-quickrun") },
+  { "lambdalisue/vim-quickrun-neovim-job" },
+  { "thinca/vim-quickrun",                         config = conf("vim-quickrun") },
 
-	{ "nvim-lualine/lualine.nvim", config = conf("lualine"), dependencies = { "folke/noice.nvim" } },
-	{ "b0o/incline.nvim", config = conf("incline") },
+  { "nvim-lualine/lualine.nvim",                   config = conf("lualine"),                            dependencies = { "folke/noice.nvim" } },
+  { "b0o/incline.nvim",                            config = conf("incline") },
 
-	{ "JoosepAlviste/nvim-ts-context-commentstring", dependencies = { "nvim-treesitter/nvim-treesitter" } },
-	{
-		"numToStr/Comment.nvim",
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-		config = conf("comment"),
-	},
-	{
-		"tyru/open-browser.vim",
-		config = conf("open-browser"),
-	},
-	{ "tyru/open-browser-github.vim", dependencies = { "tyru/open-browser.vim" } },
+  { "JoosepAlviste/nvim-ts-context-commentstring", dependencies = { "nvim-treesitter/nvim-treesitter" } },
+  {
+    "numToStr/Comment.nvim",
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+    config = conf("comment"),
+  },
+  {
+    "tyru/open-browser.vim",
+    config = conf("open-browser"),
+  },
+  { "tyru/open-browser-github.vim", dependencies = { "tyru/open-browser.vim" } },
 
-	{ "mattn/vim-sonictemplate", lazy = true, cmd = { "Tem", "Template" }, config = conf("vim-sonictemplate") },
-	{
-		"junegunn/vim-easy-align",
-		lazy = true,
-		event = "VimEnter",
-		config = conf("vim-easy-align"),
-	},
-	{ "bkad/CamelCaseMotion", lazy = true, event = "VimEnter", config = conf("CamelCaseMotion") },
-	{ "delphinus/auto-cursorline.nvim", lazy = true, event = "VimEnter", config = conf("auto-cursorline") },
-	{ "gen740/SmoothCursor.nvim", config = conf("SmoothCursor") },
-	{ "kshenoy/vim-signature" }, -- マーク位置の表示
-	{ "lewis6991/gitsigns.nvim", config = conf("gitsigns") },
-	{ "vim-jp/vimdoc-ja" },
-	{ "simeji/winresizer", config = conf("winresizer") },
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("todo-comments").setup({})
-		end,
-	},
+  { "mattn/vim-sonictemplate",      lazy = true,                               cmd = { "Tem", "Template" }, config = conf("vim-sonictemplate") },
+  {
+    "junegunn/vim-easy-align",
+    lazy = true,
+    event = "VimEnter",
+    config = conf("vim-easy-align"),
+  },
+  { "bkad/CamelCaseMotion",           lazy = true,                  event = "VimEnter", config = conf("CamelCaseMotion") },
+  { "delphinus/auto-cursorline.nvim", lazy = true,                  event = "VimEnter", config = conf("auto-cursorline") },
+  { "gen740/SmoothCursor.nvim",       config = conf("SmoothCursor") },
+  { "kshenoy/vim-signature" }, -- マーク位置の表示
+  { "lewis6991/gitsigns.nvim",        config = conf("gitsigns") },
+  { "vim-jp/vimdoc-ja" },
+  { "simeji/winresizer",              config = conf("winresizer") },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup({})
+    end,
+  },
 
-	{ "mechatroner/rainbow_csv", lazy = true, ft = { "csv" }, config = conf("rainbow_csv") },
+  { "mechatroner/rainbow_csv", lazy = true,      ft = { "csv" },             config = conf("rainbow_csv") },
 
-	{
-		"mattn/emmet-vim",
-		lazy = true,
-		ft = {
-			"html",
-			"css",
-			"php",
-			"xml",
-			"javascript",
-			"vue",
-			"typescriptreact",
-			"react",
-			"javascriptreact",
-			"markdown",
-			"astro",
-		},
-		config = conf("emmet-vim"),
-	},
+  {
+    "mattn/emmet-vim",
+    lazy = true,
+    ft = {
+      "html",
+      "css",
+      "php",
+      "xml",
+      "javascript",
+      "vue",
+      "typescriptreact",
+      "react",
+      "javascriptreact",
+      "markdown",
+      "astro",
+    },
+    config = conf("emmet-vim"),
+  },
 
-	{
-		"dhruvasagar/vim-table-mode",
-		lazy = true,
-		ft = { "markdown", "md", "mdwn", "mkd", "mkdn", "mark" },
-		config = conf("vim-table-mode"),
-	},
+  {
+    "dhruvasagar/vim-table-mode",
+    lazy = true,
+    ft = { "markdown", "md", "mdwn", "mkd", "mkdn", "mark" },
+    config = conf("vim-table-mode"),
+  },
 
-	{
-		"iamcco/markdown-preview.nvim",
-		lazy = true,
-		ft = { "markdown", "md", "mdwn", "mkd", "mkdn", "mark" },
-		build = "cd app && yarn install",
-	},
+  {
+    "iamcco/markdown-preview.nvim",
+    lazy = true,
+    ft = { "markdown", "md", "mdwn", "mkd", "mkdn", "mark" },
+    build = "cd app && yarn install",
+  },
 
-	{ "akinsho/toggleterm.nvim", version = "v2.*", config = conf("toggleterm") },
+  { "akinsho/toggleterm.nvim", version = "v2.*", config = conf("toggleterm") },
 })
