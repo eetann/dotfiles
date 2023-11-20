f19edd4a7d0a0671571c9c861b341f23cf62eecc
eetann <hideharu092@gmail.com>
2023-11-20 15:27:14 +0900

    fix: コピペ時の改行の扱いを変更

--- .config/nvim/lua/rc/base.lua
+++ .config/nvim/lua/rc/base.lua
@@ -17,12 +17,12 @@ if vim.fn.has("wsl") then
   vim.g.clipboard = {
     name = "win32yank-wsl",
     copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf"
+      ["+"] = "win32yank.exe -i",
+      ["*"] = "win32yank.exe -i"
     },
     paste = {
-      ["+"] = "win32yank.exe -o --crlf",
-      ["*"] = "win32yank.exe -o --crlf"
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf"
     },
     cache_enable = 0,
   }
