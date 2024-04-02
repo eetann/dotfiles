d47154c499a7b1bc50e05abe258c79881c3083c5
eetann <hideharu092@gmail.com>
2024-04-02 10:25:13 +0900

    feat: フォントサイズの変更

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,55 +1,55 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 local config = {}
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 local function append_table(table, other)
-  for k, v in pairs(other) do
-    table[k] = v
-  end
+	for k, v in pairs(other) do
+		table[k] = v
+	end
 end
 
 local function append_array(array, other)
-  for _, value in ipairs(other) do
-    table.insert(array, value)
-  end
+	for _, value in ipairs(other) do
+		table.insert(array, value)
+	end
 end
 
 local key_table = {
-  { key = ";", mods = "CTRL", action = "IncreaseFontSize" },
-  { key = "+", mods = "CTRL", action = "IncreaseFontSize" },
-  { key = "-", mods = "CTRL", action = "DecreaseFontSize" },
-  { key = "0", mods = "CTRL", action = "ResetFontSize" },
+	{ key = ";", mods = "CTRL", action = "IncreaseFontSize" },
+	{ key = "+", mods = "CTRL", action = "IncreaseFontSize" },
+	{ key = "-", mods = "CTRL", action = "DecreaseFontSize" },
+	{ key = "0", mods = "CTRL", action = "ResetFontSize" },
 }
 
 local target = wezterm.target_triple
 
 if target:find("darwin") then
-  local mac_key_table = {
-    { key = "n", mods = "CMD", action = { SendKey = { key = "n", mods = "ALT" } } },
-    { key = "p", mods = "CMD", action = { SendKey = { key = "p", mods = "ALT" } } },
-  }
-  append_array(key_table, mac_key_table)
+	local mac_key_table = {
+		{ key = "n", mods = "CMD", action = { SendKey = { key = "n", mods = "ALT" } } },
+		{ key = "p", mods = "CMD", action = { SendKey = { key = "p", mods = "ALT" } } },
+	}
+	append_array(key_table, mac_key_table)
 elseif target:find("linux") then
-  local linux_key_table = {
-    { key = "n", mods = "SUPER", action = { SendKey = { key = "n", mods = "ALT" } } },
-    { key = "p", mods = "SUPER", action = { SendKey = { key = "p", mods = "ALT" } } },
-  }
-  append_array(key_table, linux_key_table)
+	local linux_key_table = {
+		{ key = "n", mods = "SUPER", action = { SendKey = { key = "n", mods = "ALT" } } },
+		{ key = "p", mods = "SUPER", action = { SendKey = { key = "p", mods = "ALT" } } },
+	}
+	append_array(key_table, linux_key_table)
 else
-  config.default_domain = 'WSL:Ubuntu' -- wsl -l -v
-  local wsl_key_table = {
-    { key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom 'Clipboard' },
-  }
-  append_array(key_table, wsl_key_table)
+	config.default_domain = "WSL:Ubuntu" -- wsl -l -v
+	local wsl_key_table = {
+		{ key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
+	}
+	append_array(key_table, wsl_key_table)
 end
 
 config.font = wezterm.font("HackGen Console NF")
 config.use_ime = true
-config.font_size = 13.0
-config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.font_size = 12.0
+config.color_scheme = "Gruvbox Dark (Gogh)"
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
 config.keys = key_table
