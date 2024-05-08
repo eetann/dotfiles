59a9566307d4ea7a0206e5fba6e92aa02224823e
eetann <hideharu092@gmail.com>
2024-05-08 18:42:25 +0900

    feat: mdxハイライトとブログ用

--- .config/nvim/after/queries/markdown/injections.scm
+++ .config/nvim/after/queries/markdown/injections.scm
@@ -1,5 +1,10 @@
-; https://phelipetls.github.io/posts/mdx-syntax-highlight-treesitter-nvim/
 ; extends
+; syntax highlighting for tsx comments
+((inline) @injection.content
+  (#lua-match? @injection.content "^%s*{/*")
+  (#set! injection.language "tsx"))
+
+; https://phelipetls.github.io/posts/mdx-syntax-highlight-treesitter-nvim/
 ((inline) @injection.content
   (#lua-match? @injection.content "^%s*import")
   (#set! injection.language "tsx"))
