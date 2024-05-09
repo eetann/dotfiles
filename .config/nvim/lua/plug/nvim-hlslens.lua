d8f220d07253d4f9ea64ab4e16c1374ff8b50873
eetann <hideharu092@gmail.com>
2024-05-09 17:02:58 +0900

    fix: カーソル下検索を複数行対応

--- .config/nvim/lua/plug/nvim-hlslens.lua
+++ .config/nvim/lua/plug/nvim-hlslens.lua
@@ -8,18 +8,19 @@ vim.keymap.set("n", "N", [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua
 local hlslens = require("hlslens")
 vim.keymap.set("n", "*", function()
 	local current_word = vim.fn.expand("<cword>")
+	vim.fn.histadd("search", current_word)
 	vim.fn.setreg("/", current_word)
 	vim.opt.hlsearch = true
 	hlslens.start()
 end, opts)
 vim.keymap.set("n", "#", function()
 	local current_word = vim.fn.expand("<cword>")
-	vim.opt.hlsearch = true
-	hlslens.start()
 	vim.api.nvim_feedkeys(":%s/" .. current_word .. "//g", "n", false)
 	-- :%s/word/CURSOR/g
 	local ll = vim.api.nvim_replace_termcodes("<Left><Left>", true, true, true)
 	vim.api.nvim_feedkeys(ll, "n", false)
+	vim.opt.hlsearch = true
+	hlslens.start()
 end, opts)
 
 local function getVisualSelection()
@@ -28,7 +29,8 @@ local function getVisualSelection()
 	local text = vim.fn.getreg("v")
 	vim.fn.setreg("v", {})
 
-	text = string.gsub(text, "\n", "")
+	-- NOTE: *と#では使わない
+	-- text = string.gsub(text, "\n", "")
 	if #text > 0 then
 		return text
 	else
@@ -37,20 +39,20 @@ local function getVisualSelection()
 end
 vim.keymap.set("v", "*", function()
 	local current_word = getVisualSelection()
+	current_word = vim.fn.substitute(vim.fn.escape(current_word, "\\"), "\\n", "\\\\n", "g")
+	vim.fn.histadd("search", current_word)
+	vim.fn.setreg("/", current_word)
 	vim.opt.hlsearch = true
 	hlslens.start()
-	-- let @/ = '\V' . substitute(escape(@z, '/\'), '\n', '\\n', 'g')
-	-- print(vim.fn.escape(current_word, "\\"))
-	-- vim.fn.setreg("/", vim.fn.substitute(vim.fn.escape(current_word, "\\"), "\\n", "\\\\n", "g"))
-	vim.fn.setreg("/", current_word)
 end, opts)
 
 vim.keymap.set("v", "#", function()
 	local current_word = getVisualSelection()
-	vim.opt.hlsearch = true
-	hlslens.start()
+	current_word = vim.fn.substitute(vim.fn.escape(current_word, "\\"), "\\n", "\\\\n", "g")
 	vim.api.nvim_feedkeys(":%s/" .. current_word .. "//g", "n", false)
 	-- :%s/word/CURSOR/g
 	local ll = vim.api.nvim_replace_termcodes("<Left><Left>", true, true, true)
 	vim.api.nvim_feedkeys(ll, "n", false)
+	vim.opt.hlsearch = true
+	hlslens.start()
 end, opts)
