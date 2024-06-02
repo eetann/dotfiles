ddc5608a49031b598d00eb80e86a5c516cf5cc1d
eetann <hideharu092@gmail.com>
2024-06-02 20:06:19 +0900

    feat: ブログ

--- .textlintrc.js
+++ .textlintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
-  "plugins": {
+  plugins: {
     "@textlint/markdown": {
-      "extensions": [
-        ".mdx"
-      ]
-    }
+      extensions: [".mdx"],
+    },
   },
-  "filters": {
+  filters: {
     // NEED: textlint-filter-rule-comments
     // <!-- textlint-disable -->
     // <!-- textlint-enable -->
-    "comments": true,
+    comments: true,
     // NEED: textlint-filter-rule-allowlist
-    "allowlist": {
-      "allow": [
+    allowlist: {
+      allow: [
         "ってるよ。",
         "分報",
         "おすすめ",
@@ -23,70 +21,55 @@ module.exports = {
         "/\\{/\\* textlint-disable \\*/\\}[^]*?\\{/\\* textlint-enable \\*/\\}/m",
         // mdx用 {/* hoge */}を無視
         "/\\{/\\*[^]*?\\*/\\}/",
-        "/<Icon.*/"
-      ]
-    }
+        "/<Icon.*/",
+      ],
+    },
   },
-  "rules": {
+  rules: {
     // NEED: textlint-rule-preset-ja-technical-writing
     "preset-ja-technical-writing": {
       "sentence-length": {
-        "skipPatterns": [
+        skipPatterns: [
           // mdx用 {/* hoge */}を無視
           "/\\{/\\*[^]*?\\*/\\}/m",
           "/import.*/",
-        ]
+        ],
       },
       "max-kanji-continuous-len": {
-        "allow": [
+        allow: [
           "人類補完計画",
           "倍精度浮動小数点数",
           "自己署名証明書",
           "証明書署名要求",
           "第三者配信広告",
           "広告配信事業者",
-          "引用許可範囲"
-        ]
+          "引用許可範囲",
+        ],
       },
       "ja-no-successive-word": {
         // オノマトペを許可する
         // 制限: オノマトペを判定する方法がないため、同じカタカナの語が連続したものをオノマトペとして扱う
-        "allowOnomatopee": true,
+        allowOnomatopee: true,
         // 許可する単語
         // RegExp-like Stringを使用可能
-        "allow": [
-          "ほど",
-          "一つ",
-          "〇",
-          "…",
-          "など"
-        ]
+        allow: ["ほど", "一つ", "〇", "…", "など"],
       },
       "ja-no-mixed-period": {
-        "allowEmojiAtEnd": true,
+        allowEmojiAtEnd: true,
         // 句点文字として許可する文字列の配列
-        "allowPeriodMarks": [
-          "w",
-          "]",
-          "："
-        ]
+        allowPeriodMarks: ["w", "]", "："],
       },
       "ja-no-weak-phrase": false,
       "no-exclamation-question-mark": false,
       "no-doubled-joshi": {
-        "allow": [
-          "も",
-          "に"
-        ]
-      }
+        allow: ["ても", "も", "に"],
+      },
     },
     "preset-jtf-style": {
-      "4.3.7.山かっこ<>": false
+      "4.3.7.山かっこ<>": false,
     },
-    "prh": {
-      "rulePaths": [
-        "~/dotfiles/etc/textlint/prh.yaml"
-      ]
-    }
-  }
-}
+    prh: {
+      rulePaths: ["~/dotfiles/etc/textlint/prh.yaml"],
+    },
+  },
+};
