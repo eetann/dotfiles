718c781957c44d0f01a307a61f334760fab08a73
eetann <hideharu092@gmail.com>
2024-06-24 15:47:32 +0900

    fix: for gitsigns update

--- .config/nvim/lua/plug/gitsigns.lua
+++ .config/nvim/lua/plug/gitsigns.lua
@@ -1,10 +1,10 @@
 require("gitsigns").setup({
 	signs = {
-		add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-		change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-		delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		add = { text = "│" },
+		change = { text = "│" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
 	},
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -34,9 +34,6 @@ require("gitsigns").setup({
 		relative = "cursor",
 		row = 0,
 		col = 1,
-	},
-	yadm = {
-		enable = false,
 	},
 	on_attach = function()
 		local gs = package.loaded.gitsigns
--- .config/nvim/lua/rc/plugin.lua
+++ .config/nvim/lua/rc/plugin.lua
@@ -82,6 +82,7 @@ require("lazy").setup({
 	{ "nvimdev/lspsaga.nvim", dependencies = { "nvim-treesitter/nvim-treesitter" } },
 	{ "nvimtools/none-ls.nvim", dependencies = { "gbprod/none-ls-shellcheck.nvim" } },
 	{ "hedyhli/outline.nvim" },
+	-- TODO: https://github.com/folke/lazydev.nvim に書き換え
 	{ "folke/neodev.nvim" },
 	{ "folke/lsp-colors.nvim" },
 	{ "RRethy/vim-illuminate" },
