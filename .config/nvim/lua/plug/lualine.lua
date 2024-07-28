13c52c1f8a56d673ee2107e194d1e3b1e9c88018
eetann <hideharu092@gmail.com>
2024-07-28 18:36:25 +0900

    style: lualineに選択した文字のカウントを追加

--- .config/nvim/lua/plug/lualine.lua
+++ .config/nvim/lua/plug/lualine.lua
@@ -11,12 +11,46 @@ local function is_table_mode()
 	return "TABLE"
 end
 
+-- https://zenn.dev/glaucus03/articles/ff710d27de4e55
+local function selectionCount()
+	local mode = vim.fn.mode()
+	local start_line, end_line, start_pos, end_pos
+
+	-- 選択モードでない場合には無効
+	if not (mode:find("[vV\22]") ~= nil) then
+		return ""
+	end
+	start_line = vim.fn.line("v")
+	end_line = vim.fn.line(".")
+
+	if mode == "V" then
+		-- 行選択モードの場合は、各行全体をカウントする
+		start_pos = 1
+		end_pos = vim.fn.strlen(vim.fn.getline(end_line)) + 1
+	else
+		start_pos = vim.fn.col("v")
+		end_pos = vim.fn.col(".")
+	end
+
+	local chars = 0
+	for i = start_line, end_line do
+		local line = vim.fn.getline(i)
+		local line_len = vim.fn.strlen(line)
+		local s_pos = (i == start_line) and start_pos or 1
+		local e_pos = (i == end_line) and end_pos or line_len + 1
+		chars = chars + vim.fn.strchars(line:sub(s_pos, e_pos - 1))
+	end
+
+	local lines = math.abs(end_line - start_line) + 1
+	return tostring(lines) .. " lines, " .. tostring(chars) .. " characters"
+end
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
 		theme = "powerline",
-		component_separators = { left = " ", right = " " },
-		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {},
 		always_divide_middle = true,
 		globalstatus = true,
@@ -42,6 +76,9 @@ require("lualine").setup({
 			},
 		},
 		lualine_x = {
+			selectionCount,
+		},
+		lualine_y = {
 			{
 				require("noice").api.status.message.get_hl,
 				cond = require("noice").api.status.message.has,
@@ -61,12 +98,13 @@ require("lualine").setup({
 				cond = require("noice").api.status.search.has,
 				color = { fg = "#ff9e64" },
 			},
+			"location",
+		},
+		lualine_z = {
 			"encoding",
 			"fileformat",
 			"filetype",
 		},
-		lualine_y = {},
-		lualine_z = { "location" },
 	},
 	inactive_sections = {
 		lualine_a = {},
