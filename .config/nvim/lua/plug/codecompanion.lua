4aca63b097695777107d6ba5b8a83a96e0b3b749
eetann <hideharu092@gmail.com>
2024-08-24 17:38:48 +0900

    fix: adapterの修正

--- .config/nvim/lua/plug/codecompanion.lua
+++ .config/nvim/lua/plug/codecompanion.lua
@@ -1,11 +1,13 @@
 require("codecompanion").setup({
-	ollama = require("codecompanion.adapters").use("ollama", {
-		schema = {
-			model = {
-				default = "codellama:13b",
+	adapters = {
+		ollama = require("codecompanion.adapters").use("ollama", {
+			schema = {
+				model = {
+					default = "codellama:7b",
+				},
 			},
-		},
-	}),
+		}),
+	},
 	strategies = {
 		chat = {
 			adapter = "ollama",
@@ -17,74 +19,83 @@ require("codecompanion").setup({
 			adapter = "ollama",
 		},
 	},
-	-- default_prompts = {
-	-- 	["Explain LSP Diagnostics"] = {
-	-- 		strategy = "chat",
-	-- 		description = "Explain the LSP diagnostics for the selected code",
-	-- 		opts = {
-	-- 			index = 8,
-	-- 			default_prompt = true,
-	-- 			mapping = "<LocalLeader>cl",
-	-- 			modes = { "v" },
-	-- 			slash_cmd = "lsp",
-	-- 			auto_submit = true,
-	-- 			user_prompt = false,
-	-- 			stop_context_insertion = true,
-	-- 		},
-	-- 		prompts = {
-	-- 			{
-	-- 				role = "system",
-	-- 				content = [[あなたは専門的なコーダーであり、警告やエラーメッセージなどのコード診断のデバッグを助けることができる親切なアシスタントです。適切な場合は、シンタックスハイライトを有効にするために言語識別子をフェンスで囲んだコードブロックとしてコードスニペットで解決策を与えます。日本語で回答してください。]],
-	-- 			},
-	-- 			{
-	-- 				role = "${user}",
-	-- 				content = function(context)
-	-- 					local diagnostics = require("codecompanion.helpers.actions").get_diagnostics(
-	-- 						context.start_line,
-	-- 						context.end_line,
-	-- 						context.bufnr
-	-- 					)
-	--
-	-- 					local concatenated_diagnostics = ""
-	-- 					for i, diagnostic in ipairs(diagnostics) do
-	-- 						concatenated_diagnostics = concatenated_diagnostics
-	-- 							.. i
-	-- 							.. ". Issue "
-	-- 							.. i
-	-- 							.. "\n  - Location: Line "
-	-- 							.. diagnostic.line_number
-	-- 							.. "\n  - Severity: "
-	-- 							.. diagnostic.severity
-	-- 							.. "\n  - Message: "
-	-- 							.. diagnostic.message
-	-- 							.. "\n"
-	-- 					end
-	--
-	-- 					return "プログラミング言語は"
-	-- 						.. context.filetype
-	-- 						.. "です。diagnosticの一覧は次のとおりです。:\n\n"
-	-- 						.. concatenated_diagnostics
-	-- 				end,
-	-- 			},
-	-- 			{
-	-- 				role = "${user}",
-	-- 				contains_code = true,
-	-- 				content = function(context)
-	-- 					return "関連するコードは次のとおりです。:\n\n"
-	-- 						.. "```"
-	-- 						.. context.filetype
-	-- 						.. "\n"
-	-- 						.. require("codecompanion.helpers.actions").get_code(
-	-- 							context.start_line,
-	-- 							context.end_line,
-	-- 							{ show_line_numbers = true }
-	-- 						)
-	-- 						.. "\n```\n\n"
-	-- 				end,
-	-- 			},
-	-- 		},
-	-- 	},
-	-- },
+	actions = {
+		{
+			name = "Translate",
+			description = "LSP & Translate",
+			opts = {
+				modes = { "v" },
+			},
+			callback = function(context)
+				local agent = require("codecompanion.workflow")
+				return agent
+					.new({
+						context = context,
+						strategy = "chat",
+					})
+					:workflow({
+						{
+							role = "system",
+							start = true,
+							content = [[You are an expert coder and helpful assistant who can help debug code diagnostics, such as warning and error messages. When appropriate, give solutions with code snippets as fenced codeblocks with a language identifier to enable syntax highlighting.]],
+						},
+						{
+							role = "${user}",
+							start = true,
+							content = function(context)
+								local diagnostics = require("codecompanion.helpers.actions").get_diagnostics(
+									context.start_line,
+									context.end_line,
+									context.bufnr
+								)
+
+								local concatenated_diagnostics = ""
+								for i, diagnostic in ipairs(diagnostics) do
+									concatenated_diagnostics = concatenated_diagnostics
+										.. i
+										.. ". Issue "
+										.. i
+										.. "\n  - Location: Line "
+										.. diagnostic.line_number
+										.. "\n  - Severity: "
+										.. diagnostic.severity
+										.. "\n  - Message: "
+										.. diagnostic.message
+										.. "\n"
+								end
+
+								return "The programming language is "
+									.. context.filetype
+									.. ". This is a list of the diagnostic messages:\n\n"
+									.. concatenated_diagnostics
+							end,
+						},
+						-- {
+						-- 	role = "${user}",
+						-- 	contains_code = true,
+						-- 	start = true,
+						-- 	content = function(context)
+						-- 		return "This is the code, for context:\n\n"
+						-- 			.. "```"
+						-- 			.. context.filetype
+						-- 			.. "\n"
+						-- 			.. require("codecompanion.helpers.actions").get_code(
+						-- 				context.start_line,
+						-- 				context.end_line,
+						-- 				{ show_line_numbers = true }
+						-- 			)
+						-- 			.. "\n```\n\n"
+						-- 	end,
+						-- },
+						-- {
+						-- 	role = "${user}",
+						-- 	content = "回答を日本語に翻訳してください。",
+						-- 	auto_submit = true,
+						-- },
+					})
+			end,
+		},
+	},
 })
 
 -- Expand 'cc' into 'CodeCompanion' in the command line
