a94272e40b6171f1118dbcdf7deb45487f03e860
eetann <hideharu092@gmail.com>
2024-08-30 19:41:47 +0900

    feat: docker x fzfを追加

--- zsh/005_myfunction.zsh
+++ zsh/005_myfunction.zsh
@@ -35,7 +35,33 @@ zle -N no-magic-abbrev-expand
 bindkey " " magic-abbrev-expand
 bindkey "^x " no-magic-abbrev-expand
 
-# プラグインによる関数--------------------------------------------
+# https://github.com/junegunn/fzf/wiki/Examples#docker
+# Select a docker container to start and attach to
+function docker_start_attach() {
+  local cid
+  cid=$(docker ps -a | sed 1d | fzf -1 -q "$1" | awk '{print $1}')
+
+  [ -n "$cid" ] && docker start "$cid" && docker attach "$cid"
+}
+
+# Select a running docker container to stop
+function docker_stop() {
+  local cid
+  cid=$(docker ps | sed 1d | fzf -q "$1" | awk '{print $1}')
+
+  [ -n "$cid" ] && docker stop "$cid"
+}
+
+# # Select a docker container to remove
+function docker_rm() {
+  docker ps -a | sed 1d | fzf -q "$1" --no-sort -m --tac | awk '{ print $1 }' | xargs -r docker rm
+}
+
+# Select a docker image or images to remove
+function docker_rmi() {
+  docker images | sed 1d | fzf -q "$1" --no-sort -m --tac | awk '{ print $3 }' | xargs -r docker rmi
+}
+
 # z ×fzf
 function fz() {
   local fzf_command="fzf"
