b6670cb07a742ff5da4da60f2ea04083d1115d42
eetann <hideharu092@gmail.com>
2024-09-01 08:01:30 +0900

    feat: Oilで相対パスで開くように変更

--- .config/nvim/lua/plug/oil.lua
+++ .config/nvim/lua/plug/oil.lua
@@ -1,1 +1,12 @@
 require("oil").setup()
+
+-- oil fix relative path
+--https://github.com/stevearc/oil.nvim/issues/234
+vim.api.nvim_create_augroup("OilRelPathFix", {})
+vim.api.nvim_create_autocmd("BufLeave", {
+	group = "OilRelPathFix",
+	pattern = "oil:///*",
+	callback = function()
+		vim.cmd("cd .")
+	end,
+})
