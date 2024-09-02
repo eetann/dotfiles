fc53ee624e28d305d86ac77fba3791db5c5f4f42
eetann <hideharu092@gmail.com>
2024-09-02 18:59:48 +0900

    feat: code actionの自動化を追加

--- .config/nvim/lua/plug/lsp.lua
+++ .config/nvim/lua/plug/lsp.lua
@@ -101,6 +101,50 @@ saga.setup({
 	},
 })
 
+---@diagnostic disable: lowercase-global
+function run_code_action_only(only)
+	vim.lsp.buf.code_action({
+		context = {
+			only = { only },
+			diagnostics = {},
+		},
+		apply = true,
+	})
+end
+
+-- https://github.com/golang/tools/blob/v0.23.0/gopls/doc/vim.md#neovim-imports
+-- fro ldo
+_G.my_code_actions = function()
+	vim.lsp.buf.code_action({
+		apply = true,
+		context = { diagnostics = {} },
+		filter = function(action)
+			return action.kind == "quickfix.biome.style.useImportType"
+		end,
+	})
+	-- NOTE: buf_request_sync version
+	--
+	-- local params = vim.lsp.util.make_range_params()
+	-- params.context = { diagnostics = {} }
+	-- local kinds = { "quickfix.biome.style.useImportType" }
+	-- local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+	-- -- vim.print(result)
+	-- for _, res in pairs(result or {}) do
+	-- 	for _, r in pairs(res.result or {}) do
+	-- 		if table.in_value(kinds, r.kind) and r.edit then
+	-- 			local enc = "utf-8"
+	-- 			vim.lsp.util.apply_workspace_edit(r.edit, enc)
+	-- 		end
+	-- 	end
+	-- end
+end
+
+vim.api.nvim_create_user_command("MyQuickfix", function()
+	vim.diagnostic.setloclist()
+	vim.cmd("sleep 100ms")
+	vim.cmd("ldo call v:lua.my_code_actions()")
+end, {})
+
 require("actions-preview").setup({
 	telescope = {
 		sorting_strategy = "ascending",
@@ -195,6 +239,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		if client == nil then
 			return
 		end
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				if client.name == "biome" then
+					run_code_action_only("source.organizeImports.biome")
+				elseif client.name == "gopls" then
+					run_code_action_only("source.organizeImports.gopls")
+				end
+				require("conform").format({ bufnr = args.buf, lsp_fallback = true })
+			end,
+		})
 	end,
 })
 
