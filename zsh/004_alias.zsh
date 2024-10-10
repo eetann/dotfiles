9ab66ad0cece548b8825d0d067b1e7f148ae3351
eetann <hideharu092@gmail.com>
2024-10-10 12:23:46 +0900

    feat: nb用のエイリアスを設定

--- zsh/004_alias.zsh
+++ zsh/004_alias.zsh
@@ -1,13 +1,12 @@
 alias la='ls -F --color -al'
 alias ls='ls -F --color'
-alias mytree='tree -a -I ".git|node_modules|dist" --charset unicode'
+alias mytree='tree -a -I ".git|node_modules|dist|vendor|platforms" --charset unicode'
 alias reload="source ~/.zshrc"
 alias echopath="echo $PATH | sed -e 's/:/\n/g'"
 alias manja="man -L ja"
 alias manen="man -L en"
 alias lg='lazygit --use-config-dir="$HOME/.config/lazygit"'
 alias zd='cd ~/dotfiles'
-alias dupdate="git pull && nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'"
 case ${OSTYPE} in
   darwin*)
     alias xargs="gxargs"
@@ -15,6 +14,7 @@ case ${OSTYPE} in
     alias awk="gawk"
     ;;
 esac
+alias idea='nb e idea'
 
 # nocorrect
 alias ionic='nocorrect ionic'
