ace4623ec36f24b471bfd2b75478d3c4a6cdd400
eetann <hideharu092@gmail.com>
2024-10-16 17:03:31 +0900

    feat: mdx用のペアを追加

--- .config/nvim/lua/plug/nvim-autopairs.lua
+++ .config/nvim/lua/plug/nvim-autopairs.lua
@@ -1,1 +1,8 @@
-require("nvim-autopairs").setup({})
+local npairs = require("nvim-autopairs")
+local Rule = require("nvim-autopairs.rule")
+
+npairs.setup({})
+
+npairs.add_rules({
+	Rule("```", "```", { "mdx" }),
+})
