6147f673000241b8ed356d3b2d5237e989b1a392
eetann <hideharu092@gmail.com>
2024-10-18 18:25:34 +0900

    fix: 名称変更

--- .config/nvim/lua/lsp/laravel-lsp.lua
+++ .config/nvim/lua/lsp/laravel-lsp.lua
@@ -1,14 +1,13 @@
 local util = require("lspconfig.util")
 local configs = require("lspconfig.configs")
 
-local server_path = vim.fn.expand("~/ghq/github.com/eetann/laravel-lsp/")
+local server_path = vim.fn.expand("~/ghq/github.com/eetann/laravel-ls/")
 
-configs["laravel-lsp"] = {
+configs["laravel-ls"] = {
 	default_config = {
 		cmd = { "node", server_path .. "dist/server.js", "--stdio" },
 		filetypes = { "blade", "php" },
-		-- TODO: 後で修正
-		root_dir = util.root_pattern("composer.json", "package.json"),
+		root_dir = util.root_pattern("composer.json"),
 		settings = {},
 	},
 }
