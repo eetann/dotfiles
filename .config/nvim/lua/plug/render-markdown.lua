1288b1b369dc9e87329b9bc17308d2d1770c0f92
eetann <hideharu092@gmail.com>
2024-10-22 18:43:57 +0900

    style: render-blockの行全体ハイライトをブロック全体へ変更

--- .config/nvim/lua/plug/render-markdown.lua
+++ .config/nvim/lua/plug/render-markdown.lua
@@ -1,1 +1,12 @@
+require("render-markdown").setup({
+	heading = {
+		width = "block",
+		left_pad = 0,
+		right_pad = 4,
+		icons = {},
+	},
+	code = {
+		width = "block",
+	},
+})
 vim.keymap.set("n", "<Space>sm", ":RenderMarkdown toggle<CR>")
