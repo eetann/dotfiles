5064e8641965f7dbc01b94cbc70f4b4e19af6b39
eetann <hideharu092@gmail.com>
2024-10-25 12:55:10 +0900

    fix: VSCodeだったらtmuxを開かないように設定

--- zsh/000_path.zsh
+++ zsh/000_path.zsh
@@ -19,20 +19,22 @@ if [[ -z "$TMUX" && -z "$VIM" && $- == *l* ]] ; then
     export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
   fi
 
-  # get the IDs
-  ID="`tmux list-sessions`"
-  if [[ -z "$ID" ]]; then
-    tmux new-session
-  fi
-  create_new_session="Create New Session"
-  ID="$ID\n${create_new_session}:"
-  ID="`echo $ID | fzf | cut -d: -f1`"
-  if [[ "$ID" = "${create_new_session}" ]]; then
-    tmux new-session
-  elif [[ -n "$ID" ]]; then
-    tmux attach-session -t "$ID"
-  else
-    :  # Start terminal normally
+  if [[ $TERM_PROGRAM != "vscode" ]] ; then
+    # get the IDs
+    ID="`tmux list-sessions`"
+    if [[ -z "$ID" ]]; then
+      tmux new-session
+    fi
+    create_new_session="Create New Session"
+    ID="$ID\n${create_new_session}:"
+    ID="`echo $ID | fzf | cut -d: -f1`"
+    if [[ "$ID" = "${create_new_session}" ]]; then
+      tmux new-session
+    elif [[ -n "$ID" ]]; then
+      tmux attach-session -t "$ID"
+    else
+      :  # Start terminal normally
+    fi
   fi
 fi
 
