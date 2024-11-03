127efd49d96f5409890b3e1cb9886327b9ed30f7
eetann <hideharu092@gmail.com>
2024-11-03 15:09:35 +0900

    fix: markdown-previewのビルドエラー対策

--- .config/nvim/lua/rc/plugin.lua
+++ .config/nvim/lua/rc/plugin.lua
@@ -241,9 +241,8 @@ require("lazy").setup({
 	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && yarn install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown", "plantuml", "pu" }
+		build = function()
+			vim.fn["mkdp#util#install"]()
 		end,
 		ft = { "markdown", "pu", "plantuml" },
 		config = function()
