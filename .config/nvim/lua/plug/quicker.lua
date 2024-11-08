aae63b2222d1dee70e8b63481736918992f8cd0e
eetann <hideharu092@gmail.com>
2024-11-08 19:02:17 +0900

    feat: quickerのキーマップを追加

--- .config/nvim/lua/plug/quicker.lua
+++ .config/nvim/lua/plug/quicker.lua
@@ -2,4 +2,9 @@ require("quicker").setup({
 	opts = {
 		buflisted = true,
 	},
+	keys = {
+		{ "ge", "<cmd>lua require('quicker').expand()<CR>", desc = "Expand quickfix content" },
+		{ "gE", "<cmd>lua require('quicker').collapse()<CR>", desc = "Collapse quickfix content" },
+		{ "gr", "<cmd>lua require('quicker').refresh()<CR>", desc = "Refresh quickfix content" },
+	},
 })
