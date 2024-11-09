a059473f877fdaa01a07df10662fc29ec10b1873
eetann <hideharu092@gmail.com>
2024-11-09 09:34:26 +0900

    feat: クオートの中でもautopairになるように変更

--- .config/nvim/lua/plug/nvim-autopairs.lua
+++ .config/nvim/lua/plug/nvim-autopairs.lua
@@ -1,8 +1,15 @@
 local npairs = require("nvim-autopairs")
 local Rule = require("nvim-autopairs.rule")
 
-npairs.setup({})
+npairs.setup({
+	-- カーソルの後ろに何があってもautopairsを有効にする
+	ignored_next_char = "",
+})
 
 npairs.add_rules({
 	Rule("```", "```", { "mdx" }),
+	-- デフォルトだと "not_add_quote_inside_quote" によりクオートの中でautopairできないので
+	-- 自前で定義にする
+	Rule('"', '"'),
+	Rule("'", "'"),
 })
