9626e255abba0ef80246758f1d8306dc333dec8e
eetann <hideharu092@gmail.com>
2025-01-05 11:33:39 +0900

    refactor: 使わなくなったプラグインの設定ファイルを削除

--- .config/nvim/lua/plugins/completion/ultisnips.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"SirVer/ultisnips",
-	dependencies = { "honza/vim-snippets" },
-	event = { "VeryLazy" },
-	init = function()
-		vim.g.UltiSnipsSnippetDirectories = { "~/dotfiles/.config/nvim/snippet" }
-		vim.g.UltiSnipsJumpForwardTrigger = "<Plug>(ultisnips_jump_forward)"
-	end,
-}
