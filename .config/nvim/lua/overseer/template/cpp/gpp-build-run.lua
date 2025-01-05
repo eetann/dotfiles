5b1afe1e330f643dc3a3a8f6fb6fd57d4d91e7dc
eetann <hideharu092@gmail.com>
2025-01-05 16:53:26 +0900

    refactor: コメントアウトの削除

--- .config/nvim/lua/overseer/template/cpp/gpp-build-run.lua
+++ .config/nvim/lua/overseer/template/cpp/gpp-build-run.lua
@@ -1,14 +1,11 @@
 return {
 	name = "g++ build and run",
 	builder = function()
-		-- Full path to current file (see :help expand())
 		local file = vim.fn.expand("%:p")
 		local outfile = vim.fn.expand("%:p:r") .. ".out"
 		return {
 			cmd = { outfile },
 			components = {
-				-- Note that since we're using the "raw task parameters" format for the dependency,
-				-- we don't have to define a separate build task.
 				{
 					"dependencies",
 					task_names = {
