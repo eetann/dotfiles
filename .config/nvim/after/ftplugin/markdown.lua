934015829dc217d7a55139fb810723d00c7c0378
eetann <hideharu092@gmail.com>
2025-01-08 14:38:08 +0900

    feat: 見出しつきwikilinkのgfとリンク化を実装

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -79,3 +79,52 @@ end, {
 	expr = true,
 	silent = true,
 })
+
+-- ref: https://vi.stackexchange.com/questions/9344/open-markdown-filename-under-cursor-like-gf-and-jump-to-the-section
+vim.keymap.set("n", "gf", function()
+	local cfile = vim.fn.expand("<cfile>")
+	-- ([^#]*) -> ファイル名 -> %1
+	-- (#+) -> 見出しのシャープ -> %2
+	-- ([^#]*) -> 見出しテキスト -> %3
+	local arg = cfile:gsub("([^#]*)(#+)([^#]*)", function(filename, hashes, heading)
+		-- 見出しテキストの`-`をエスケープ
+		local escaped_heading = heading:gsub("-", ".")
+		return "+/" .. hashes .. "\\ " .. escaped_heading .. " " .. filename
+	end)
+	vim.cmd("edit " .. arg)
+end, { desc = "gfを拡張: 見出しリンクも辿れる", buffer = true })
+
+-- 現在の箇所をwikilink化
+vim.keymap.set("n", "<space>mw", function()
+	-- 現在の位置をmで記録
+	vim.cmd("mark z")
+
+	-- 現在のテキストの見出しに移動
+	vim.fn.search("^#", "b")
+
+	-- 見出しテキストを取得してエスケープする
+	local heading_link = ""
+	if vim.fn.line(".") ~= 1 then
+		local line = vim.api.nvim_get_current_line()
+		heading_link = line:gsub("(#+)%s+([^#]*)", function(hashes, heading)
+			-- 見出しテキストをエスケープ
+			local escaped_heading = heading:gsub("[][%s^$*(){}|]", "-")
+			return hashes .. escaped_heading
+		end)
+		-- 一行目に移動
+		vim.api.nvim_win_set_cursor(0, { 1, 0 })
+	end
+
+	-- タイトルの取得
+	local line = vim.api.nvim_get_current_line()
+	local title = line:match("#%s+([^#]*)")
+
+	-- リンクの取得
+	local current_file = vim.fn.expand("%")
+	local link = "[[" .. current_file .. heading_link .. "|" .. title .. "]]"
+	vim.fn.setreg("+", link)
+	vim.print("yank '" .. link .. "'")
+
+	-- 元の位置に戻る
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("`z", true, false, true), "in", false)
+end, { desc = "現在行のwikilinkを取得", buffer = true })
