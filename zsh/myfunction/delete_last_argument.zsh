9ad730b85db05a0dbbe4dbc09aca0215306c9969
eetann <hideharu092@gmail.com>
2025-01-10 12:13:06 +0900

    feat: ブログ用スニペットを追加

--- zsh/myfunction/delete_last_argument.zsh
+++ zsh/myfunction/delete_last_argument.zsh
@@ -1,15 +1,15 @@
 function delete_last_argument() {
   # シェルのパースに基づいて配列化
   # https://zsh.sourceforge.io/Doc/Release/Expansion.html#Parameter-Expansion-Flags:~:text=into%20words%20using%20shell%20parsing%20to%20find%20the%20words
-  local args=("${(z)BUFFER}")
+  local tokens=("${(z)BUFFER}")
 
-  if (( 2 <= ${#args} )); then
+  if (( 2 <= ${#tokens} )); then
     # 配列の取り出し
     # https://zsh.sourceforge.io/Doc/Release/Expansion.html#Parameter-Expansion:~:text=%24%7Bname%3Aoffset%3Alength%7D
-    args=("${args:0:$#args-1}")
+    tokens=("${tokens:0:$#tokens-1}")
     # 配列を半角スペースで結合
     # https://zsh.sourceforge.io/Doc/Release/Expansion.html#Parameter-Expansion:~:text=the%20parenthesised%20group.-,j%3Astring%3A,-Join%20the%20words
-    BUFFER="${(j: :)args} "
+    BUFFER="${(j: :)tokens} "
   fi
 }
 
