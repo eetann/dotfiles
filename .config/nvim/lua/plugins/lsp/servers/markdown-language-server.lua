6f2b331e322191cbad14aac14cbb119cce4524d2
eetann <hideharu092@gmail.com>
2025-01-10 18:57:48 +0900

    feat: 自作LSPの設定を追加

---
+++ .config/nvim/lua/plugins/lsp/servers/markdown-language-server.lua
@@ -0,0 +1,17 @@
+local util = require("lspconfig.util")
+local configs = require("lspconfig.configs")
+
+-- ここにこのリポジトリのパスを書いておく
+local server_path = vim.fn.expand("~/ghq/github.com/eetann/markdown-language-server/")
+
+configs["markdown-language-server"] = {
+	default_config = {
+		cmd = { "node", server_path .. "packages/server/bin/markdown-language-server.cjs", "--stdio" },
+		filetypes = { "markdown" },
+		root_dir = util.root_pattern(".git"),
+		settings = {},
+	},
+}
+
+---@type lspconfig.Config
+return {}
