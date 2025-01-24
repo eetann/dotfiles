648c715e77c126c44edcf7bbb3e25cf0f9264710
eetann <hideharu092@gmail.com>
2025-01-24 19:02:33 +0900

    feat: assertの補完も追加

--- .config/nvim/lua/plugins/lsp/lazydev.lua
+++ .config/nvim/lua/plugins/lsp/lazydev.lua
@@ -6,6 +6,7 @@ return {
 			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 			{ path = "overseer.nvim", words = { "overseer" } },
 			{ path = "${3rd}/busted/library", words = { "it%(", "describe%(" } },
+			{ path = "${3rd}/luassert/library", words = { "assert" } },
 		},
 	},
 }
