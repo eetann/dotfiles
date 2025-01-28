cf86c4fe5503dc74635cbb665c9a4d05c83d8ee4
eetann <hideharu092@gmail.com>
2025-01-28 19:00:08 +0900

    feat: luaの設定を追加

--- .config/nvim/lua/plugins/lsp/servers/lua_ls.lua
+++ .config/nvim/lua/plugins/lsp/servers/lua_ls.lua
@@ -2,6 +2,11 @@
 return {
 	settings = {
 		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using
+				-- (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
 			completion = {
 				callSnippet = "Replace",
 			},
