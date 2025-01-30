f97b11b7a3b7553791b1ed91e7fea855d1d39316
eetann <hideharu092@gmail.com>
2025-01-30 15:40:41 +0900

    fix: プロンプトエラーの解消

--- zsh/myfunction/osc133.zsh
+++ zsh/myfunction/osc133.zsh
@@ -1,18 +1,20 @@
 _prompt_executing=""
+
 function after_command() {
   local ret="$?"
-  if test "$_prompt_executing" != "0" then
+  if [[ "$_prompt_executing" -ne 0 ]]; then
     _PROMPT_SAVE_PS1="$PS1"
     _PROMPT_SAVE_PS2="$PS2"
     PS1=$'%{\e]133;P;k=i\a%}'$PS1$'%{\e]133;B\a\e]122;> \a%}'
     PS2=$'%{\e]133;P;k=s\a%}'$PS2$'%{\e]133;B\a%}'
   fi
-  if test "$_prompt_executing" != "" then
+  if [[ -n "$_prompt_executing" ]]; then
     printf "\033]133;D;%s;aid=%s\007" "$ret" "$$"
   fi
   printf "\033]133;A;cl=m;aid=%s\007" "$$"
   _prompt_executing=0
 }
+
 function before_command() {
   PS1="$_PROMPT_SAVE_PS1"
   PS2="$_PROMPT_SAVE_PS2"
