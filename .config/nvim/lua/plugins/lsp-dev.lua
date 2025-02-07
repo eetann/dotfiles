85606f7e031ba0a13acf45a6c7e265fa5ad31779
eetann <hideharu092@gmail.com>
2025-02-07 17:06:11 +0900

    chore: 遅延読み込み設定を追加

--- .config/nvim/lua/plugins/lsp-dev.lua
+++ .config/nvim/lua/plugins/lsp-dev.lua
@@ -1,8 +1,10 @@
+---@type LazyPluginSpec
 return {
 	dir = "~/ghq/github.com/eetann/lsp-dev.nvim",
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 	},
+	cmd = { "LspDev" },
 	keys = {
 		{ "<space>ls", ":LspDev showLog<CR>" },
 		{ "<space>lc", ":LspDev changeLogLevel<CR>" },
