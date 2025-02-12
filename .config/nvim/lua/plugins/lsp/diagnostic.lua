8a1e596bb42022545c53c246f5ae8fa20ecc1bfb
eetann <hideharu092@gmail.com>
2025-02-12 15:33:08 +0900

    fix: 競合する設定を修正

--- .config/nvim/lua/plugins/lsp/diagnostic.lua
+++ .config/nvim/lua/plugins/lsp/diagnostic.lua
@@ -1,11 +1,11 @@
 vim.diagnostic.config({
 	severity_sort = true,
-	virtual_text = {
-		prefix = "●",
-		format = function(diagnostic)
-			return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
-		end,
-	},
+	-- virtual_text = {
+	-- 	prefix = "●",
+	-- 	format = function(diagnostic)
+	-- 		return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
+	-- 	end,
+	-- },
 	signs = {
 		text = {
 			[vim.diagnostic.severity.ERROR] = "",
--- .config/nvim/lua/plugins/lsp/tiny-inline-diagnostic.lua
+++ .config/nvim/lua/plugins/lsp/tiny-inline-diagnostic.lua
@@ -1,6 +1,6 @@
 return {
 	"rachartier/tiny-inline-diagnostic.nvim",
-	event = { "LspAttach" },
+	event = "VeryLazy",
 	priority = 1000, -- needs to be loaded in first
 	config = function()
 		require("tiny-inline-diagnostic").setup({
