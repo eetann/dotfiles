60d9b27c446229d424ba573a3d9ec268233628bd
eetann <hideharu092@gmail.com>
2025-02-20 09:24:35 +0900

    refactor: 不要なプラグインの設定階層を削除

--- .config/nvim/lua/plugins/completion/index.lua
+++ .config/nvim/lua/plugins/completion/index.lua
@@ -1,36 +1,34 @@
 ---@module "lazy"
----@type LazyPluginSpec[]
+---@type LazyPluginSpec
 return {
-	{
-		"saghen/blink.cmp",
-		event = { "InsertEnter", "CmdLineEnter" },
-		dependencies = {
-			{ import = "plugins.completion.luasnip" },
-			{
-				"Kaiser-Yang/blink-cmp-dictionary",
-				dependencies = { "nvim-lua/plenary.nvim" },
-			},
+	"saghen/blink.cmp",
+	event = { "InsertEnter", "CmdLineEnter" },
+	dependencies = {
+		{ import = "plugins.completion.luasnip" },
+		{
+			"Kaiser-Yang/blink-cmp-dictionary",
+			dependencies = { "nvim-lua/plenary.nvim" },
 		},
-		-- use a release tag to download pre-built binaries
-		version = "*",
-		---@module "blink.cmp"
-		---@type blink.cmp.Config
-		opts = {
-			keymap = require("plugins.completion.mappings"),
-
-			snippets = { preset = "luasnip" },
-			sources = require("plugins.completion.sources"),
-
-			-- 見た目
-			appearance = {
-				use_nvim_cmp_as_default = true,
-				nerd_font_variant = "normal",
-			},
-			completion = {
-				documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = "single" } },
-			},
-			signature = { window = { border = "single" } },
-		},
-		opts_extend = { "sources.default" },
 	},
+	-- use a release tag to download pre-built binaries
+	version = "*",
+	---@module "blink.cmp"
+	---@type blink.cmp.Config
+	opts = {
+		keymap = require("plugins.completion.mappings"),
+
+		snippets = { preset = "luasnip" },
+		sources = require("plugins.completion.sources"),
+
+		-- 見た目
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "normal",
+		},
+		completion = {
+			documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = "single" } },
+		},
+		signature = { window = { border = "single" } },
+	},
+	opts_extend = { "sources.default" },
 }
