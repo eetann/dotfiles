f69ca6e2fe5f0e0b7720e3e5ba0771a4f0add417
eetann <hideharu092@gmail.com>
2025-03-09 21:45:04 +0900

    refactro: senpaiプラグイン設定にオプション型注釈を追加
    型注釈により、設定の静的解析を支援します。開発時のコード補完と
    エラー検出が改善されます。また、未使用のディレクトリ指定を
    コメントアウトし、設定がより明確になります。

--- .config/nvim/lua/plugins/senpai.lua
+++ .config/nvim/lua/plugins/senpai.lua
@@ -2,7 +2,12 @@
 ---@type LazyPluginSpec
 return {
 	"eetann/senpai.nvim",
+	-- dir = "~/ghq/github.com/eetann/senpai.nvim",
 	dependencies = { "vim-denops/denops.vim" },
 	event = { "VeryLazy" },
-	opts = {},
+	---@module "senpai"
+	---@type senpai.Config
+	opts = {
+		-- provider = "openrouter",
+	},
 }
