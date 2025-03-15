88d47e95e8967786e81f024be510d94d63ee2e6b
eetann <hideharu092@gmail.com>
2025-03-15 22:04:11 +0900

    feat(denops): add shared server settings

--- .config/nvim/lua/plugins/denops.lua
+++ .config/nvim/lua/plugins/denops.lua
@@ -1,10 +1,27 @@
+---@module "lazy"
+---@type LazyPluginSpec[]
 return {
-	"vim-denops/denops.vim",
-	-- 開発プラグイン側でclone・setupしているが
-	-- ヘルプを見たいのでプラグインマネージャー側でも追加
-	cmd = "DenopsHelp",
-	config = function()
-		-- プラグインとしては使わないのでsetupはしない
-		vim.api.nvim_create_user_command("DenopsHelp", ":belowright vertical help denops", {})
-	end,
+	{
+		"vim-denops/denops.vim",
+		-- プラグイン側でclone・setupしているが
+		-- ヘルプを見たいのでプラグインマネージャー側でも追加
+		cmd = "DenopsHelp",
+		init = function()
+			-- vim.g.denops_server_addr = "127.0.0.1:32123"
+		end,
+		config = function()
+			-- プラグインとしては使わないのでsetupはしない
+			vim.api.nvim_create_user_command("DenopsHelp", ":belowright vertical help denops", {})
+		end,
+	},
+	{
+		"vim-denops/denops-shared-server.vim",
+		dependencies = { "vim-denops/denops.vim" },
+		cmd = "DenopsSharedServerInstall",
+		config = function()
+			vim.api.nvim_create_user_command("DenopsSharedServerInstall", function()
+				vim.fn["denops_shared_server#install"]()
+			end, {})
+		end,
+	},
 }
