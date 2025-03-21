20bf4d64b3e5b2b0b5c3d9ea9b9a97aaa4469027
eetann <hideharu092@gmail.com>
2025-03-21 19:02:05 +0900

    refactor: delete condition for code block autopairs

--- .config/nvim/lua/plugins/senpai.lua
+++ .config/nvim/lua/plugins/senpai.lua
@@ -3,7 +3,6 @@
 return {
 	-- "eetann/senpai.nvim",
 	dir = "~/ghq/github.com/eetann/senpai.nvim",
-	dependencies = { "vim-denops/denops.vim" },
 	keys = {
 		{ "<space>ss", "<Cmd>Senpai toggleChat<CR>" },
 	},
