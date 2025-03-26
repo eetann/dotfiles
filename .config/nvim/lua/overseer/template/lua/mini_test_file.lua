4c2dcbbf43deca7afcf1027df024add45c54cc82
eetann <hideharu092@gmail.com>
2025-03-26 09:24:26 +0900

    feat: WIP change minitest

--- .config/nvim/lua/overseer/template/lua/mini_test_file.lua
+++ .config/nvim/lua/overseer/template/lua/mini_test_file.lua
@@ -2,13 +2,24 @@
 return {
 	name = "MiniTest run this file",
 	builder = function()
-		vim.cmd("lua MiniTest.run_file()")
+		local file = vim.fn.expand("%")
 		---@type overseer.TaskDefinition
 		return {
-			cmd = { "echo" },
-			args = { "MiniTest" },
-			cwd = vim.fn.expand("%:p:h"),
+			cmd = { "nvim" },
+			args = {
+				"--headless",
+				"--noplugin",
+				"-u",
+				"./scripts/test/minimal_init.lua",
+				"-c",
+				[["lua MiniTest.run_file(']]
+					.. file
+					.. [[', {execute = { reporter = MiniTest.gen_reporter.stdout() }})"]],
+			},
+			cwd = vim.fn.getcwd(),
 			components = {
+				{ "on_exit_set_status", success_codes = { 0 } },
+				{ "on_complete_notify", statuses = {} },
 				{ "on_output_quickfix", open_on_exit = "failure" },
 				"default",
 			},
