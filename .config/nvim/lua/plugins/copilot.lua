750d43185db85be0b2f9c494310ad346d5231e3b
eetann <hideharu092@gmail.com>
2025-04-30 19:10:59 +0900

    feat(nvim): Copilotのsuggestion機能を有効化
    Copilotプラグインのsuggestion機能を有効化し、関連するキーマップを設定しました。
    これにより、コード編集中にCopilotからの提案を受け入れ、操作できるようになります。

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -4,7 +4,17 @@ return {
 	"zbirenbaum/copilot.lua",
 	lazy = true,
 	opts = {
-		suggestion = { enabled = false },
+		suggestion = {
+			enabled = true,
+			keymap = {
+				accept = "<C-g>a",
+				accept_word = false,
+				accept_line = "<C-g>l",
+				prev = "<C-g>[",
+				next = "<C-g>]",
+				dismiss = "<C-g>e",
+			},
+		},
 		panel = { enabled = false },
 	},
 }
