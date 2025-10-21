d505c0b0858f1a6f0a016b73f4213651212ab0f2
eetann <hideharu092@gmail.com>
2025-10-21 18:35:54 +0900

    fix(editprompt): 起動方法間違ってたので修正

--- .config/nvim/lua/config/editprompt.lua
+++ .config/nvim/lua/config/editprompt.lua
@@ -5,20 +5,22 @@ end
 vim.opt.wrap = true
 
 vim.keymap.set("n", "<Space>x", function()
-	vim.cmd("silent write")
+	vim.cmd("update")
 	-- バッファの内容を取得
 	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 	local content = table.concat(lines, "\n")
 
 	-- editpromptコマンドを実行
 	vim.system(
-		{ "bun", "run", vim.fn.expand("~/ghq/github.com/eetann/editprompt/dist/index.js"), "--", content },
+		{ "node", vim.fn.expand("~/ghq/github.com/eetann/editprompt/dist/index.js"), "--", content },
+		-- { "editprompt", "--", content },
 		{ text = true },
 		function(obj)
 			vim.schedule(function()
 				if obj.code == 0 then
 					-- 成功したらバッファを空にする
 					vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
+					vim.cmd("silent write")
 				else
 					-- 失敗したら通知
 					vim.notify("editprompt failed: " .. (obj.stderr or "unknown error"), vim.log.levels.ERROR)
