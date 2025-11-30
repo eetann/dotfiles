935574253ea4646b7581040b571f3891a85ac360
eetann <hideharu092@gmail.com>
2025-12-01 08:47:53 +0900

    chore: add dependencies

--- .config/nvim/lua/plugins/idyank.lua
+++ .config/nvim/lua/plugins/idyank.lua
@@ -2,6 +2,10 @@
 ---@type LazyPluginSpec
 return {
   dir = "~/ghq/github.com/eetann/idyank.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter-context",
+  },
   keys = {
     { "<Space>y", "<Cmd>Idyank yank<CR>" },
   },
