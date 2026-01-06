b4a575d3769fca249d8123c05979184962186251
eetann <hideharu092@gmail.com>
2026-01-06 09:37:02 +0900

    refactor(nvim): oxc_lspが実行できないときの警告を解消

--- .config/nvim/after/lsp/oxc_lsp.lua
+++ .config/nvim/after/lsp/oxc_lsp.lua
@@ -1,5 +1,12 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { "node_modules/.bin/oxc_language_server" },
+  cmd = function(root_dir)
+    local cmd = root_dir .. "/node_modules/.bin/oxc_language_server"
+    if vim.fn.executable(cmd) == 1 then
+      return { cmd }
+    end
+    ---@diagnostic disable-next-line: return-type-mismatch
+    return nil
+  end,
   root_markers = { ".oxlintrc.json", ".oxlintrc.jsonc" },
 }
