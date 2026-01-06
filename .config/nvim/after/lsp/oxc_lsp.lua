5920f85200d3eee485a2e44f7e25ba051fecf319
eetann <hideharu092@gmail.com>
2026-01-06 09:41:33 +0900

    Revert "refactor(nvim): oxc_lspが実行できないときの警告を解消"
    This reverts commit b4a575d3769fca249d8123c05979184962186251.

--- .config/nvim/after/lsp/oxc_lsp.lua
+++ .config/nvim/after/lsp/oxc_lsp.lua
@@ -1,12 +1,5 @@
 ---@type vim.lsp.Config
 return {
-  cmd = function(root_dir)
-    local cmd = root_dir .. "/node_modules/.bin/oxc_language_server"
-    if vim.fn.executable(cmd) == 1 then
-      return { cmd }
-    end
-    ---@diagnostic disable-next-line: return-type-mismatch
-    return nil
-  end,
+  cmd = { "node_modules/.bin/oxc_language_server" },
   root_markers = { ".oxlintrc.json", ".oxlintrc.jsonc" },
 }
