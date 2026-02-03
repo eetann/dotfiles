6346a42769e867a7be931c45a280a883a258bd3a
eetann <hideharu092@gmail.com>
2026-02-04 08:34:37 +0900

    feat(wezterm): タイトルバーの非表示

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -213,6 +213,7 @@ config.font_rules = {
 }
 config.use_ime = true
 config.color_scheme = "Gruvbox Dark (Gogh)"
+config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
 config.keys = key_table
