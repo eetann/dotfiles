b641940957a991ffb883b1af9ea5bbf088bcf282
eetann <hideharu092@gmail.com>
2026-04-07 15:42:37 +0900

    feat(nvim): oil.nvimでファイル削除にgomiを使うようにした

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,34 +1,34 @@
+---@module "lazy"
+---@type LazyPluginSpec
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   cond = not vim.g.vscode,
   event = { "VeryLazy" },
-  opts = {},
-  config = function()
-    require("oil").setup({
-      view_options = {
-        show_hidden = true,
-      },
-      keymaps = {
-        ["q"] = { "actions.close", mode = "n" },
-      },
-    })
-
-    -- oil fix relative path
-    -- https://github.com/stevearc/oil.nvim/issues/234
-    vim.api.nvim_create_augroup("OilRelPathFix", {})
-    vim.api.nvim_create_autocmd("BufLeave", {
-      group = "OilRelPathFix",
-      pattern = "oil:///*",
-      callback = function()
-        vim.cmd("cd .")
+  keys = {
+    {
+      "<Space>go",
+      function()
+        require("oil").open()
       end,
-    })
-    vim.keymap.set("n", "<Space>go", function()
-      require("oil").open()
-    end, { desc = "Oil current buffer's directory" })
-    vim.keymap.set("n", "<Space>gO", function()
-      require("oil").open(".")
-    end, { desc = "Oil ." })
-  end,
+      desc = "Oil current buffer's directory",
+    },
+    {
+      "<Space>gO",
+      function()
+        require("oil").open(".")
+      end,
+      desc = "Oil .",
+    },
+  },
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    delete_to_trash = true,
+    trash_command = "gomi",
+    keymaps = {
+      ["q"] = { "actions.close", mode = "n" },
+    },
+  },
 }
