7868dc75b1229d87cd9f46734bfd8a4de93dc27b
eetann <hideharu092@gmail.com>
2026-08-19 09:54:50 +0900

    feat(nvim): mdechoの引数修正

--- .config/nvim/lua/overseer/template/markdown/2_mdecho.lua
+++ .config/nvim/lua/overseer/template/markdown/2_mdecho.lua
@@ -3,7 +3,7 @@ return {
   name = "mdecho",
   condition = { filetype = "markdown" },
   generator = function(opts)
-    local file = vim.fn.expand("%:p")
+    local file = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":~:.")
     if file:match(".*local/share/nvim/scratch/.*markdown") then
       return "使用不可"
     end
