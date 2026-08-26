b4af8448a77e2e91ffdd5462c071167215418beb
eetann <hideharu092@gmail.com>
2026-08-26 09:05:12 +0000

    feat(zsh): WSLでblenderエイリアスを追加

--- zsh/myfunction/godot.zsh
+++ zsh/myfunction/godot.zsh
@@ -1,4 +1,5 @@
 if [[ "$(uname -r)" == *microsoft* ]]; then
   alias godot='"/mnt/c/Program Files/godot/godot.exe"'
   alias godot-console='"/mnt/c/Program Files/godot/godot-console.exe"'
+  alias blender='"/mnt/c/Program Files (x86)/Steam/steamapps/common/Blender/blender.exe"'
 fi
